2b8a0a3c210a9c731d2e47a42f6275c5958175c0
Raymond Forbes <rforbes@mozilla.com>
2017-03-06 01:30:47 -0800

    updates to webgl corpus manager

--- corpman/WebGLCorpman.py
+++ corpman/WebGLCorpman.py
@@ -25,47 +25,45 @@ class WebGLCorpusManager(corpman.CorpusManager):
 
     def _init_fuzzer(self, _):
         self.enable_harness()
+        self.webgl_frames = []
 
-    def _fuzz(self, frames):
+    def _fuzz(self):
 
         aggression = random.randint(1, 10)
+
         for _ in range(aggression):
-            #fuzz_choice = random.randint(0, 3)
+            #fuzz_choice = random.randint(0, 2)
             fuzz_choice = 1
             if fuzz_choice == 0: # shuffle frames
-                self._munge_setup(frames)
+                exit(0)
+                #self._munge_setup()
             elif fuzz_choice == 1:
-                self._munge_buffer_data(frames)
+                self._munge_buffer_data()
             elif fuzz_choice == 2:
-                print "hold"
+                exit(0)
 
-    def _munge_buffer_data(self, frames):
-        buffer_constants_list = ['__GL_STATIC_DRAW', '__GL_STREAM_DRAW', '__GL_DYNAMIC_DRAW', '__GL_ARRAY_BUFFER', '__GL_ELEMENT_ARRAY_BUFFER', '__GL_BUFFER_SIZE', '__GL_BUFFER_USAGE']
+    def _munge_buffer_data(self):
+        buffer_constants_list = ['__GL_STATIC_DRAW',
+                                 '__GL_STREAM_DRAW',
+                                 '__GL_DYNAMIC_DRAW',
+                                 '__GL_ARRAY_BUFFER',
+                                 '__GL_ELEMENT_ARRAY_BUFFER',
+                                 '__GL_BUFFER_SIZE',
+                                 '__GL_BUFFER_USAGE']
 
-        for frame in frames:
+        for frameset in self.webgl_frames["frames"]:
             temp_frames = []
-            for item in frame:
-                if item[1] == "bufferData":
-                    new_frame = copy.deepcopy(item)
+            for frame in frameset:
+                if frame[1] == "bufferData":
+                    new_frame = copy.deepcopy(frame)
                     new_frame[-1][-1] = buffer_constants_list[random.randint(0, len(buffer_constants_list) - 1)]
                     temp_frames.append(new_frame)
-
             if len(temp_frames) > 0:
                 for item in temp_frames:
-                    frame.append(item)
+                    frameset.append(item)
 
-    def _duplicate_frames(self, frames):
-        total_frames = len(frames)
-        chunk = random.randint(1, total_frames)
-        starting_point = random.randint(0, (total_frames) - chunk)
-        insertion_point = random.randint(0, (total_frames))
-
-        working_frames = frames[starting_point:starting_point + chunk]
-
-        frames.insert(insertion_point, working_frames)
-
-    def _munge_setup(self, frames):
-
+    def _munge_setup(self):
+    
         aggression = random.randint(1, 10)
 
         for _ in range(aggression):
@@ -73,39 +71,35 @@ class WebGLCorpusManager(corpman.CorpusManager):
             fuzz_choice = random.randint(0, 3)
 
             if fuzz_choice == 0: # shuffle frames
-                random.shuffle(frames)
+                random.shuffle(self.webgl_frames)
             elif fuzz_choice == 1: # duplicate frames
-                self._duplicate_frames(frames)
+                self._duplicate_frames()
             elif fuzz_choice == 2: # delete frames
-                removed_frame = random.randint(1, len(frames) - 1)
-                del frames[removed_frame]
+                removed_frame = random.randint(1, len(self.webgl_frames) - 1)
+                del self.webgl_frames[removed_frame]
+
+    def _duplicate_frames(self):
+        total_frames = len(self.webgl_frames)
+        chunk = random.randint(1, total_frames)
+        starting_point = random.randint(0, (total_frames) - chunk)
+        insertion_point = random.randint(0, (total_frames))
+
+            # working_frames = frames[starting_point:starting_point + chunk]
 
 
     def _generate(self, test_case, redirect_page, mime_type=None):
         with open(test_case.template.file_name) as json_data:
-            data = json.load(json_data)
+            self.webgl_frames = json.load(json_data)
 
-        temp_data = copy.deepcopy(data)
-        temp_frames = temp_data["frames"]
-
-        self._fuzz(temp_frames)
-        temp_data["frames"] = temp_frames
+        self._fuzz()
 
         with open(os.path.join('..', 'grizzly-private', 'resources', 'webgl', 'utilities.js'), 'r') as fp:
-            utility_data = fp.read()
-        utilities_data_url = self.to_data_url(utility_data, mime_type="appliation/javascript")
+            utilities_data_url = self.to_data_url(fp.read(), mime_type="appliation/javascript")
 
         with open(os.path.join('..', 'grizzly-private', 'resources', 'webgl', 'webgl-rr.js'), 'r') as fp:
-            webglrr_data = fp.read()
-        webglrr_data_url = self.to_data_url(webglrr_data, mime_type="appliation/javascript")
+            webglrr_data_url = self.to_data_url(fp.read(), mime_type="appliation/javascript")
 
-        with tempfile.NamedTemporaryFile() as outfile:
-            json.dump(temp_data, outfile, indent=4)
-            outfile.seek(0)
-            fuzzed_file_data = outfile.read()
-            print outfile.name
-
-        fuzzedfile_data_url = self.to_data_url(fuzzed_file_data, mime_type="appliation/json")
+        fuzzedfile_data_url = self.to_data_url(json.dumps(self.webgl_frames, indent=4), mime_type="appliation/json")
         # prepare data for playback
         data = "\n".join([
             "<!DOCTYPE html>",
@@ -158,6 +152,6 @@ class WebGLCorpusManager(corpman.CorpusManager):
             "</html>"
             ])
 
-        print test_case.landing_page
         test_case.add_testfile(corpman.TestFile(test_case.landing_page, data))
+
         
