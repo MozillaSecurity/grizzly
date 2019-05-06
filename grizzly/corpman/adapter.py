d1b3748dea87b29fa23dd712ed6651ba9b785b0a
Tyson Smith <tyson.w.smith@gmail.com>
2019-05-06 13:50:17 -0700

    Make Adapter an Abstract Base Class

--- grizzly/corpman/adapter.py
+++ grizzly/corpman/adapter.py
@@ -2,72 +2,134 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import abc
 import os
+
+import six
 
 from .storage import TestFile
 
+__all__ = ("Adapter",)
 __author__ = "Tyson Smith"
 __credits__ = ["Tyson Smith"]
 
 
+@six.add_metaclass(abc.ABCMeta)
 class Adapter(object):
     HARNESS_FILE = os.path.join(os.path.dirname(__file__), "harness.html")
     IGNORE_UNSERVED = True  # Only report test cases with served content
-    NAME = None  # must be set by subclass
+    NAME = None  # must be set to a unique 'str' by subclass
     ROTATION_PERIOD = 10  # iterations per input file before switching
     TEST_DURATION = 30  # maximum execution time per test
 
     #############################
-    # Builtins do NOT overload!
+    # Built-ins do NOT overload!
     #############################
 
     def __init__(self):
         if self.NAME is None:
             raise NotImplementedError("NAME member must be set by subclass")
+        assert isinstance(self.NAME, str), "NAME must be a 'str'"
         self._harness = None
         self.fuzz = dict()
         self.monitor = None
 
-
     def cleanup(self):
+        """Automatically called once at shutdown.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         if self._harness is not None:
             self._harness.close()
             self._harness = None
         self.shutdown()
 
-
     def enable_harness(self, file_path=None):
+        """Enable use of a harness during fuzzing. By default no harness is used.
+
+        Args:
+            file_path (str or None): Path to file to use as a harness. If None the default harness is used.
+
+        Returns:
+            None
+        """
         if self._harness is not None:
             self._harness.close()
         self._harness = TestFile.from_file(
             self.HARNESS_FILE if file_path is None else file_path,
             "grizzly_fuzz_harness.html")
 
-
     def get_harness(self):
-        return self._harness
+        """Get the harness. Used internally by Grizzly.
 
+        Args:
+            None
+
+        Returns:
+            grizzly.corpman.TestFile: The current harness
+        """
+        return self._harness
 
     #############################
     # Methods to overload
     #############################
 
+    @abc.abstractmethod
     def generate(self, testcase, input_file, server_map):
-        raise NotImplementedError("Must be implemented by subclass")
+        """Automatically called. Populate testcase here.
 
+        Args:
+            testcase (grizzly.corpman.TestCase): TestCase intended to be populated
+            input_file (grizzly.corpman.InputFile): Contains input data
+            server_map (grizzly.corpman.iomanager.ServerMap): A ServerMap
+
+        Returns:
+            None
+        """
 
     def on_served(self, testcase, served):
-        pass
+        """Optional. Automatically called after a test case is successfully served.
 
+        Args:
+            testcase (grizzly.corpman.TestCase): TestCase that was served
+            served (list): A list of file names served from testcase
+
+        Returns:
+            None
+        """
 
     def on_timeout(self, testcase, served):
-        # by default call on_served
+        """Optional. Automatically called if timeout occurs attempting to serve a test case.
+
+        Args:
+            testcase (grizzly.corpman.TestCase): TestCase that was served
+            served (list): A list of file names served from testcase
+
+        Returns:
+            None
+        """
         self.on_served(testcase, served)
 
-
     def setup(self, server_map):
-        pass
+        """Optional. Automatically called once at startup.
 
+        Args:
+            server_map (grizzly.corpman.iomanager.ServerMap): A ServerMap
+
+        Returns:
+            None
+        """
 
     def shutdown(self):
-        pass
+        """Optional. Automatically called once at shutdown.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
