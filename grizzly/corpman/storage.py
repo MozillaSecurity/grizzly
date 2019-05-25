3758c85568b0de5196d91f2fc877635845df00d2
Tyson Smith <tyson.w.smith@gmail.com>
2019-05-24 18:22:12 -0700

    [test] Convert storage tests to pytest

--- grizzly/corpman/storage.py
+++ grizzly/corpman/storage.py
@@ -9,8 +9,14 @@ import os
 import shutil
 import tempfile
 
+__all__ = ("InputFile", "TestCase", "TestFile", "TestFileExists")
 __author__ = "Tyson Smith"
 __credits__ = ["Tyson Smith"]
+
+
+class TestFileExists(RuntimeError):
+    """Raised when adding a TestFile to a TestCase that has an existing TestFile with the same name"""
+
 
 class InputFile(object):
     CACHE_LIMIT = 0x100000  # 1MB
@@ -19,26 +25,21 @@ class InputFile(object):
         self.extension = None
         self.file_name = file_name
         self._fp = None
-
         if not os.path.isfile(file_name):
-            raise IOError("File does %r does not exist" % self.file_name)
-
+            raise IOError("File %r does not exist" % (self.file_name,))
         # TODO: add kwarg to set self.extension?
         if "." in self.file_name:
             self.extension = os.path.splitext(self.file_name)[-1].lstrip(".")
-
 
     def _cache_data(self):
         self._fp = tempfile.SpooledTemporaryFile(max_size=self.CACHE_LIMIT)
         with open(self.file_name, "rb") as src_fp:
             shutil.copyfileobj(src_fp, self._fp, 0x10000)  # 64KB
 
-
     def close(self):
         if self._fp is not None:
             self._fp.close()
         self._fp = None
-
 
     def get_data(self):
         """
@@ -47,13 +48,11 @@ class InputFile(object):
 
         returns input file data from file.read()
         """
-
         if self._fp is None:
             self._cache_data()
         self._fp.seek(0)
         # TODO: add size limit
         return self._fp.read()
-
 
     def get_fp(self):
         if self._fp is None:
@@ -67,16 +66,15 @@ TestFileMap = namedtuple("TestFileMap", "meta optional required")
 class TestCase(object):
     def __init__(self, landing_page, redirect_page, adapter_name, input_fname=None):
         self.adapter_name = adapter_name
+        self.input_fname = input_fname  # file that was used to create the test case
         self.landing_page = landing_page
         self.redirect_page = redirect_page
-        self.input_fname = input_fname  # file that was used to create the test case
         self._env_vars = dict()  # environment variables
+        self._existing_paths = list()  # file paths in use
         self._files = TestFileMap(
             meta=list(),  # environment files such as prefs.js, etc...
             optional=list(),
             required=list())
-        self._existing_paths = set()  # file paths in use
-
 
     def _add(self, target, test_file):
         """
@@ -84,20 +82,17 @@ class TestCase(object):
         """
         assert isinstance(test_file, TestFile), "only accepts TestFiles"
         if test_file.file_name in self._existing_paths:
-            raise RuntimeError("file exists in test %r" % test_file.file_name)
-        self._existing_paths.add(test_file.file_name)
+            raise TestFileExists("%r exists in test" % (test_file.file_name,))
+        self._existing_paths.append(test_file.file_name)
         target.append(test_file)
-
 
     def add_meta(self, meta_file):
         self._add(self._files.meta, meta_file)
-
 
     def add_environ_var(self, name, value):
         assert isinstance(name, str)
         assert value is None or isinstance(value, str)
         self._env_vars[name] = value
-
 
     def add_file(self, test_file, required=True):
         if required:
@@ -105,18 +100,21 @@ class TestCase(object):
         else:
             self._add(self._files.optional, test_file)
 
-
     def add_from_data(self, data, file_name, encoding="UTF-8", required=True):
-        self.add_file(
-            TestFile.from_data(data=data, file_name=file_name, encoding=encoding),
-            required=required)
-
+        tfile = TestFile.from_data(data=data, file_name=file_name, encoding=encoding)
+        try:
+            self.add_file(tfile, required=required)
+        except TestFileExists:
+            tfile.close()
+            raise
 
     def add_from_file(self, input_file, file_name, required=True):
-        self.add_file(
-            TestFile.from_file(input_file=input_file, file_name=file_name),
-            required=required)
-
+        tfile = TestFile.from_file(input_file=input_file, file_name=file_name)
+        try:
+            self.add_file(tfile, required=required)
+        except TestFileExists:
+            tfile.close()
+            raise
 
     def dump(self, log_dir, include_details=False):
         """
@@ -133,7 +131,6 @@ class TestCase(object):
         # save test files to log_dir
         for test_file in self._files.required + self._files.optional:
             test_file.dump(log_dir)
-
         # save test case files and meta data including:
         # adapter used, input file, environment info and files
         if include_details:
@@ -149,7 +146,6 @@ class TestCase(object):
             # save meta files
             for meta_file in self._files.meta:
                 meta_file.dump(log_dir)
-
             # WARNING: test_info.txt and env_vars.txt are deprecated and will be removed
             # TODO: remove this section
             with open(os.path.join(log_dir, "test_info.txt"), "w") as out_fp:
@@ -166,17 +162,14 @@ class TestCase(object):
                         else:
                             out_fp.write("%s=%s\n" % (env_var, env_val))
 
-
     def cleanup(self):
         # close all the test files
         for file_group in self._files:
             for test_file in file_group:
                 test_file.close()
 
-
     def get_optional(self):
         return [x.file_name for x in self._files.optional]
-
 
     def env_vars(self):
         return ["=".join((k, v)) for k, v in self._env_vars.items() if v is not None]
@@ -189,7 +182,7 @@ class TestFile(object):
     __slots__ = ("_fp", "file_name")
 
     def __init__(self, file_name):
-        self._fp = tempfile.SpooledTemporaryFile(max_size=self.CACHE_LIMIT, prefix='grz_tf_')
+        self._fp = tempfile.SpooledTemporaryFile(max_size=self.CACHE_LIMIT, prefix="grz_tf_")
         # XXX: This is a naive fix for a larger path issue
         if "\\" in file_name:
             file_name = file_name.replace("\\", "/")
@@ -197,17 +190,14 @@ class TestFile(object):
             file_name = file_name.lstrip("/")
         self.file_name = os.path.normpath(file_name)  # name including path relative to wwwroot
 
-
     def clone(self):
         cloned = TestFile(self.file_name)
         self._fp.seek(0)
         shutil.copyfileobj(self._fp, cloned._fp, self.XFER_BUF)  # pylint: disable=protected-access
         return cloned
 
-
     def close(self):
         self._fp.close()
-
 
     @property
     def data(self):
@@ -219,7 +209,6 @@ class TestFile(object):
         self._fp.seek(pos)
         return data
 
-
     def dump(self, path):
         target_path = os.path.join(path, os.path.dirname(self.file_name))
         if not os.path.isdir(target_path):
@@ -227,7 +216,6 @@ class TestFile(object):
         self._fp.seek(0)
         with open(os.path.join(path, self.file_name), "wb") as dst_fp:
             shutil.copyfileobj(self._fp, dst_fp, self.XFER_BUF)
-
 
     @classmethod
     def from_data(cls, data, file_name, encoding="UTF-8"):
@@ -239,14 +227,12 @@ class TestFile(object):
                 t_file.write(data.encode(encoding))
         return t_file
 
-
     @classmethod
     def from_file(cls, input_file, file_name):
         t_file = cls(file_name=file_name)
         with open(input_file, "rb") as src_fp:
             shutil.copyfileobj(src_fp, t_file._fp, cls.XFER_BUF)  # pylint: disable=protected-access
         return t_file
-
 
     def write(self, data):
         self._fp.write(data)
--- grizzly/corpman/test_storage.py
+++ grizzly/corpman/test_storage.py
@@ -2,241 +2,239 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# pylint: disable=protected-access
+
 import os
-import shutil
-import tempfile
-import unittest
 
-from .storage import InputFile, TestCase, TestFile
+import pytest
+
+from .storage import InputFile, TestCase, TestFile, TestFileExists
 
 
-class TestCaseTests(unittest.TestCase):
-    def setUp(self):
-        self.tdir = tempfile.mkdtemp(prefix="tc_tests")
+def test_testcase_01(tmp_path):
+    """test empty TestCase"""
+    l_page = "land.html"
+    r_page = "redirect.html"
+    adpt_name = "test-adapter"
+    tcase = TestCase(l_page, r_page, adpt_name)
+    try:
+        assert tcase.landing_page == l_page
+        assert tcase.redirect_page == r_page
+        assert tcase.adapter_name == adpt_name
+        assert tcase.input_fname is None
+        assert not tcase._files.meta
+        assert not tcase._files.optional
+        assert not tcase._files.required
+        assert not tcase._env_vars
+        assert not tcase.get_optional()
+        tcase.dump(str(tmp_path))
+        assert not os.listdir(str(tmp_path))
+        tcase.dump(str(tmp_path), include_details=True)
+        assert "test_info.json" in os.listdir(str(tmp_path))
+        assert "test_info.txt" in os.listdir(str(tmp_path))  # deprecated
+    finally:
+        tcase.cleanup()
 
-    def tearDown(self):
-        if os.path.isdir(self.tdir):
-            shutil.rmtree(self.tdir)
+def test_testcase_02(tmp_path):
+    """test TestCase with TestFiles"""
+    tcase = TestCase("land_page.html", "redirect.html", "test-adapter", input_fname="testinput.bin")
+    try:
+        in_file = tmp_path / "testfile1.bin"
+        in_file.write_bytes(b"test_req")
+        tcase.add_from_file(str(in_file), "testfile1.bin")
+        with pytest.raises(TestFileExists) as exc:
+            tcase.add_from_file(str(in_file), "testfile1.bin")
+        assert "'testfile1.bin' exists in test" in str(exc)
+        with pytest.raises(TestFileExists) as exc:
+            tcase.add_from_data("test", "testfile1.bin")
+        assert "'testfile1.bin' exists in test" in str(exc)
+        tcase.add_from_data("test_nreq", "test_dir/testfile2.bin", required=False)
+        tcase.add_from_data("test_blah", "/testfile3.bin")
+        tcase.add_from_data("test_windows", "\\\\dir\\file.bin")
+        opt_files = tcase.get_optional()
+        assert len(opt_files) == 1
+        assert os.path.join("test_dir", "testfile2.bin") in opt_files
+        tcase.dump(str(tmp_path), include_details=True)
+        assert os.path.isdir(os.path.join(str(tmp_path), "test_dir"))
+        assert os.path.isfile(os.path.join(str(tmp_path), "test_info.json"))
+        assert os.path.isfile(os.path.join(str(tmp_path), "test_info.txt"))
+        with open(os.path.join(str(tmp_path), "test_info.txt"), "r") as test_fp:
+            assert "testinput.bin" in test_fp.read()
+        assert os.path.isfile(os.path.join(str(tmp_path), "testfile1.bin"))
+        with open(os.path.join(str(tmp_path), "testfile1.bin"), "r") as test_fp:
+            assert test_fp.read() == "test_req"
+        assert os.path.isfile(os.path.join(str(tmp_path), "test_dir", "testfile2.bin"))
+        with open(os.path.join(str(tmp_path), "test_dir", "testfile2.bin"), "r") as test_fp:
+            assert test_fp.read() == "test_nreq"
+        assert os.path.isfile(os.path.join(str(tmp_path), "testfile3.bin"))
+        with open(os.path.join(str(tmp_path), "testfile3.bin"), "r") as test_fp:
+            assert test_fp.read() == "test_blah"
+        assert os.path.isfile(os.path.join(str(tmp_path), "dir", "file.bin"))
+        with open(os.path.join(str(tmp_path), "dir", "file.bin"), "r") as test_fp:
+            assert test_fp.read() == "test_windows"
+    finally:
+        tcase.cleanup()
 
-    def test_01(self):
-        "test empty TestCase"
-        l_page = "land.html"
-        r_page = "redirect.html"
-        adpt_name = "test-adapter"
-        tc = TestCase(l_page, r_page, adpt_name)
-        self.addCleanup(tc.cleanup)
-        self.assertEqual(tc.landing_page, l_page)
-        self.assertEqual(tc.redirect_page, r_page)
-        self.assertEqual(tc.adapter_name, adpt_name)
-        self.assertIsNone(tc.input_fname)
-        self.assertFalse(tc._files.meta)  # pylint: disable=protected-access
-        self.assertFalse(tc._files.optional)  # pylint: disable=protected-access
-        self.assertFalse(tc._files.required)  # pylint: disable=protected-access
-        self.assertFalse(tc._env_vars)  # pylint: disable=protected-access
-        self.assertFalse(tc.get_optional())
-        tc.dump(self.tdir)
-        self.assertFalse(os.listdir(self.tdir))
-        tc.dump(self.tdir, include_details=True)
-        self.assertIn("test_info.txt", os.listdir(self.tdir))
-
-    def test_02(self):
-        "test TestCase with TestFiles"
-        tf1 = TestFile.from_data("test_req", "testfile1.bin")
-        tfd = TestFile.from_data("test_dup", "testfile1.bin")
-        self.addCleanup(tfd.close)
-        tf2 = TestFile.from_data("test_nreq", "test_dir/testfile2.bin")
-        tf3 = TestFile.from_data("test_blah", "/testfile3.bin")
-        tf4 = TestFile.from_data("test_windows", "\\\\dir\\file.bin")
-        tc = TestCase("land_page.html", "redirect.html", "test-adapter", input_fname="testinput.bin")
-        self.addCleanup(tc.cleanup)
-        tc.add_file(tf1)
-        with self.assertRaises(RuntimeError):
-            tc.add_file(tfd)
-        tc.add_file(tf2, required=False)
-        tc.add_file(tf3)
-        tc.add_file(tf4)
-        opt_files = tc.get_optional()
-        self.assertEqual(len(opt_files), 1)
-        self.assertIn(os.path.join("test_dir", "testfile2.bin"), opt_files)
-        tc.dump(self.tdir, include_details=True)
-        self.assertTrue(os.path.isdir(os.path.join(self.tdir, "test_dir")))
-        self.assertTrue(os.path.isfile(os.path.join(self.tdir, "test_info.json")))
-        self.assertTrue(os.path.isfile(os.path.join(self.tdir, "test_info.txt")))
-        with open(os.path.join(self.tdir, "test_info.txt"), "r") as test_fp:
-            self.assertIn("testinput.bin", test_fp.read())
-        self.assertTrue(os.path.isfile(os.path.join(self.tdir, "testfile1.bin")))
-        with open(os.path.join(self.tdir, "testfile1.bin"), "r") as test_fp:
-            self.assertEqual(test_fp.read(), "test_req")
-        self.assertTrue(os.path.isfile(os.path.join(self.tdir, "test_dir", "testfile2.bin")))
-        with open(os.path.join(self.tdir, "test_dir", "testfile2.bin"), "r") as test_fp:
-            self.assertEqual(test_fp.read(), "test_nreq")
-        self.assertTrue(os.path.isfile(os.path.join(self.tdir, "testfile3.bin")))
-        with open(os.path.join(self.tdir, "testfile3.bin"), "r") as test_fp:
-            self.assertEqual(test_fp.read(), "test_blah")
-        self.assertTrue(os.path.isfile(os.path.join(self.tdir, "dir", "file.bin")))
-        with open(os.path.join(self.tdir, "dir", "file.bin"), "r") as test_fp:
-            self.assertEqual(test_fp.read(), "test_windows")
-
-    def test_03(self):
-        "test TestCase add_meta()"
-        tc = TestCase("land_page.html", "redirect.html", "test-adapter")
-        self.addCleanup(tc.cleanup)
+def test_testcase_03(tmp_path):
+    """test TestCase add_meta()"""
+    tcase = TestCase("land_page.html", "redirect.html", "test-adapter")
+    try:
+        dmp_path = tmp_path / "dmp_test"
+        dmp_path.mkdir()
+        meta_file = dmp_path / "metafile.bin"
         meta_data = b"foobar"
-        meta_file = "metafile.bin"
-        tc.add_meta(TestFile.from_data(meta_data, meta_file))
-        dmp_dir = os.path.join(self.tdir, "dmp_test")
-        os.mkdir(dmp_dir)
-        tc.dump(dmp_dir, include_details=True)
-        dmp_contents = os.listdir(dmp_dir)
-        self.assertIn(meta_file, dmp_contents)
-        with open(os.path.join(dmp_dir, meta_file), "rb") as test_fp:
-            self.assertEqual(test_fp.read(), meta_data)
+        tcase.add_meta(TestFile.from_data(meta_data, meta_file.name))
+        tcase.dump(str(dmp_path), include_details=True)
+        assert meta_file.is_file()
+        with meta_file.open("rb") as test_fp:
+            assert test_fp.read() == meta_data
+    finally:
+        tcase.cleanup()
 
-    def test_04(self):
-        "test TestCase add_environ_var() and env_vars()"
-        tc = TestCase("land_page.html", "redirect.html", "test-adapter")
-        self.addCleanup(tc.cleanup)
-        tc.add_environ_var("TEST_ENV_VAR", "1")
-        self.assertEqual(len(tc.env_vars()), 1)
-        tc.add_environ_var("TEST_NONE", None)
-        self.assertEqual(len(tc.env_vars()), 1)
-        self.assertEqual(len(tc._env_vars), 2)  # pylint: disable=protected-access
-        dmp_dir = os.path.join(self.tdir, "dmp_test")
-        os.mkdir(dmp_dir)
-        tc.dump(dmp_dir, include_details=True)
-        dmp_contents = os.listdir(dmp_dir)
-        self.assertIn("env_vars.txt", dmp_contents)
-        with open(os.path.join(dmp_dir, "env_vars.txt"), "r") as test_fp:
+def test_testcase_04(tmp_path):
+    """test TestCase add_environ_var() and env_vars()"""
+    tcase = TestCase("land_page.html", "redirect.html", "test-adapter")
+    try:
+        tcase.add_environ_var("TEST_ENV_VAR", "1")
+        assert len(tcase.env_vars()) == 1
+        tcase.add_environ_var("TEST_NONE", None)
+        assert len(tcase.env_vars()) == 1
+        assert len(tcase._env_vars) == 2
+        dmp_path = tmp_path / "dmp_test"
+        dmp_path.mkdir()
+        tcase.dump(str(dmp_path), include_details=True)
+        env_file = dmp_path / "env_vars.txt"
+        assert env_file.is_file()
+        with env_file.open("r") as test_fp:
             data = test_fp.read()
-        self.assertIn("TEST_ENV_VAR=1\n", data)
-        self.assertIn("TEST_NONE=\n", data)
+        assert "TEST_ENV_VAR=1\n" in data
+        assert "TEST_NONE=\n" in data
+    finally:
+        tcase.cleanup()
 
+def test_inputfile_01():
+    """test InputFile with non-existing file"""
+    with pytest.raises(IOError) as exc:
+        InputFile(os.path.join("foo", "bar", "none"))
+    assert "File 'foo/bar/none' does not exist" in str(exc)
 
-class InputFileTests(unittest.TestCase):
-    def setUp(self):
-        self.tdir = tempfile.mkdtemp(prefix="if_tests")
-
-    def tearDown(self):
-        if os.path.isdir(self.tdir):
-            shutil.rmtree(self.tdir)
-
-    def test_01(self):
-        "test InputFile object"
-        # non-existing file
-        with self.assertRaises(IOError):
-            InputFile(os.path.join("foo", "bar", "none"))
-        # existing file
-        t_file = os.path.join(self.tdir, "testfile.bin")
-        with open(t_file, "w") as test_fp:
-            test_fp.write("test")
-        in_file = InputFile(t_file)
-        self.addCleanup(in_file.close)
-        self.assertEqual(in_file.extension, "bin")
-        self.assertEqual(in_file.file_name, t_file)
-        self.assertIsNone(in_file._fp)  # pylint: disable=protected-access
-        self.assertEqual(in_file.get_data(), b"test")
-        self.assertIsNotNone(in_file._fp)  # pylint: disable=protected-access
+def test_inputfile_02(tmp_path):
+    """test InputFile object"""
+    tfile = tmp_path / "testfile.bin"
+    tfile.write_bytes(b"test")
+    in_file = InputFile(str(tfile))
+    try:
+        assert in_file.extension == "bin"
+        assert in_file.file_name == str(tfile)
+        assert in_file._fp is None
+        assert in_file.get_data() == b"test"
+        assert in_file._fp is not None
         in_file.close()
-        self.assertIsNone(in_file._fp)  # pylint: disable=protected-access
-        self.assertEqual(in_file.get_fp().read(), b"test")
-        self.assertIsNotNone(in_file._fp)  # pylint: disable=protected-access
+        assert in_file._fp is None
+        assert in_file.get_fp().read() == b"test"
+        assert in_file._fp is not None
+    finally:
         in_file.close()
 
+def test_testfile_01():
+    """test simple TestFile"""
+    tfile = TestFile("test_file.txt")
+    try:
+        assert tfile.file_name == "test_file.txt"
+        assert not tfile._fp.closed
+        tfile.close()
+        assert tfile._fp.closed
+    finally:
+        tfile.close()
 
-class TestFileTests(unittest.TestCase):
-    def setUp(self):
-        self.tdir = tempfile.mkdtemp(prefix="tf_tests")
+def test_testfile_02(tmp_path):
+    """test TestFile.write() and TestFile.dump()"""
+    out_file = tmp_path / "test_file.txt"
+    tfile = TestFile(out_file.name)
+    try:
+        tfile.write(b"foo")
+        assert not out_file.is_file()
+        tfile.dump(str(tmp_path))
+        assert out_file.is_file()
+        with out_file.open("r") as in_fp:
+            assert in_fp.read() == "foo"
+        tfile.write(b"bar")
+        tfile.dump(str(tmp_path))
+        with out_file.open("r") as in_fp:
+            assert in_fp.read() == "foobar"
+    finally:
+        tfile.close()
 
-    def tearDown(self):
-        if os.path.isdir(self.tdir):
-            shutil.rmtree(self.tdir)
+def test_testfile_03(tmp_path):
+    """test TestFile.dump() file with nested path"""
+    file_path = "test/dir/path/file.txt"
+    tfile = TestFile(file_path)
+    try:
+        out_file = tmp_path / file_path
+        tfile.write(b"foo")
+        assert not out_file.is_file()
+        tfile.dump(str(tmp_path))
+        assert out_file.is_file()
+    finally:
+        tfile.close()
 
-    def test_01(self):
-        "test simple TestFile"
-        tf = TestFile("test_file.txt")
-        self.addCleanup(tf.close)
+def test_testfile_04(tmp_path):
+    """test TestFile.from_data()"""
+    # TODO: different encodings
+    tfile = TestFile.from_data("foo", "test_file.txt")
+    try:
+        out_file = tmp_path / "test_file.txt"
+        tfile.dump(str(tmp_path))
+        assert out_file.is_file()
+        with out_file.open("r") as in_fp:
+            assert in_fp.read() == "foo"
+    finally:
+        tfile.close()
 
-        self.assertEqual(tf.file_name, "test_file.txt")
-        self.assertFalse(tf._fp.closed)  # pylint: disable=protected-access
-        tf.close()
-        self.assertTrue(tf._fp.closed)  # pylint: disable=protected-access
+def test_testfile_05(tmp_path):
+    """test TestFile.from_file()"""
+    in_file = tmp_path / "infile.txt"
+    in_file.write_bytes(b"foobar")
+    tfile = TestFile.from_file(str(in_file), "outfile.txt")
+    try:
+        out_file = tmp_path / "outfile.txt"
+        tfile.dump(str(tmp_path))
+        assert out_file.is_file()
+        with out_file.open("r") as in_fp:
+            assert in_fp.read() == "foobar"
+    finally:
+        tfile.close()
 
-    def test_02(self):
-        "test write() and dump()"
-        tf = TestFile("test_file.txt")
-        self.addCleanup(tf.close)
-
-        out_file = os.path.join(self.tdir, "test_file.txt")
-        tf.write(b"foo")
-        self.assertFalse(os.path.isfile(out_file))
-        tf.dump(self.tdir)
-        self.assertTrue(os.path.isfile(out_file))
-        with open(out_file, "r") as in_fp:
-            self.assertEqual(in_fp.read(), "foo")
-        tf.write(b"bar")
-        tf.dump(self.tdir)
-        with open(out_file, "r") as in_fp:
-            self.assertEqual(in_fp.read(), "foobar")
-
-    def test_03(self):
-        "test dump() file with nested path"
-        file_path = "test/dir/path/file.txt"
-        tf = TestFile(file_path)
-        self.addCleanup(tf.close)
-
-        out_file = os.path.join(self.tdir, file_path)
-        tf.write(b"foo")
-        self.assertFalse(os.path.isfile(out_file))
-        tf.dump(self.tdir)
-        self.assertTrue(os.path.isfile(out_file))
-
-    def test_04(self):
-        "test from_data()"
-        # TODO: different encodings
-        tf = TestFile.from_data("foo", "test_file.txt")
-        self.addCleanup(tf.close)
-
-        out_file = os.path.join(self.tdir, "test_file.txt")
-        tf.dump(self.tdir)
-        self.assertTrue(os.path.isfile(out_file))
-        with open(out_file, "r") as in_fp:
-            self.assertEqual(in_fp.read(), "foo")
-
-    def test_05(self):
-        "test from_file()"
-        _fd, in_file = tempfile.mkstemp(dir=self.tdir)
-        os.close(_fd)
-        with open(in_file, "w+b") as in_fp:
-            in_fp.write(b"foobar")
-        tf = TestFile.from_file(in_file, "test_file.txt")
-        self.addCleanup(tf.close)
-
-        out_file = os.path.join(self.tdir, "test_file.txt")
-        tf.dump(self.tdir)
-        self.assertTrue(os.path.isfile(out_file))
-        with open(out_file, "r") as in_fp:
-            self.assertEqual(in_fp.read(), "foobar")
-
-    def test_06(self):
-        "test clone()"
-        tf1 = TestFile("test_file.txt")
-        self.addCleanup(tf1.close)
-
+def test_testfile_06(tmp_path):
+    """test TestFile.clone()"""
+    out_file = tmp_path / "test_file.txt"
+    tf1 = TestFile(out_file.name)
+    try:
         tf1.write(b"foobar")
-        tf2 = tf1.clone()
-        tf2.write(b"test")
-        self.addCleanup(tf1.close)
+        try:
+            tf2 = tf1.clone()
+            tf2.write(b"test")
+            assert tf1.file_name == tf2.file_name
+            assert tf1._fp != tf2._fp
+            tf2.dump(str(tmp_path))
+            assert out_file.is_file()
+            with out_file.open("r") as in_fp:
+                assert in_fp.read() == "foobartest"
+        finally:
+            tf2.close()
+        tf1.dump(str(tmp_path))
+        assert out_file.is_file()
+        with out_file.open("r") as in_fp:
+            assert in_fp.read() == "foobar"
+    finally:
+        tf1.close()
 
-        self.assertEqual(tf1.file_name, tf2.file_name)
-        self.assertNotEqual(tf1._fp, tf2._fp)  # pylint: disable=protected-access
-
-        out_file = os.path.join(self.tdir, "test_file.txt")
-        tf1.dump(self.tdir)
-        self.assertTrue(os.path.isfile(out_file))
-        with open(out_file, "r") as in_fp:
-            self.assertEqual(in_fp.read(), "foobar")
-
-        tf2.dump(self.tdir)
-        self.assertTrue(os.path.isfile(out_file))
-        with open(out_file, "r") as in_fp:
-            self.assertEqual(in_fp.read(), "foobartest")
+def test_testfile_07(tmp_path):
+    """test TestFile.data()"""
+    in_file = tmp_path / "infile.txt"
+    in_file.write_bytes(b"foobar")
+    tfile = TestFile.from_file(str(in_file), "outfile.txt")
+    try:
+        assert tfile.data == b"foobar"
+    finally:
+        tfile.close()
