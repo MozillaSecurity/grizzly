81c5757d249b27d6e6fa42ec27fcecf352dba4eb
Tyson Smith <tyson.w.smith@gmail.com>
2019-05-27 09:58:55 -0700

    [tests] Convert TargetMonitor tests to pytest

--- grizzly/target/test_target_monitor.py
+++ grizzly/target/test_target_monitor.py
@@ -2,42 +2,31 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import os
-import tempfile
-import unittest
 
 from .target_monitor import TargetMonitor
 
-
-class TargetMonitorTests(unittest.TestCase):
-
-    def test_01(self):
-        "test a basic monitor"
-        class _BasicMonitor(TargetMonitor):
-            # pylint: disable=no-self-argument,protected-access
-            def clone_log(_, log_id, offset=0):
-                tmp_fd, log_file = tempfile.mkstemp(
-                    suffix="_log.txt",
-                    prefix="test_")
-                os.close(tmp_fd)
-                with open(log_file, "wb") as log_fp:
-                    log_fp.write(b"test")
-                return log_file
-            def is_healthy(_):
-                return True
-            def is_running(_):
-                return True
-            @property
-            def launches(_):
-                return 1
-            def log_length(_, log_id):
-                return 100
-        mon = _BasicMonitor()
-
-        test_log = mon.clone_log("test_log", offset=0)
-        self.addCleanup(os.remove, test_log)
-        self.assertTrue(os.path.isfile(test_log))
-        self.assertTrue(mon.is_healthy())
-        self.assertTrue(mon.is_running())
-        self.assertEqual(mon.launches, 1)
-        self.assertEqual(mon.log_data("test_log"), b"test")
-        self.assertEqual(mon.log_length("test_log"), 100)
+def test_target_monitor_01(tmp_path):
+    """test a basic TargetMonitor"""
+    class _BasicMonitor(TargetMonitor):
+        # pylint: disable=no-self-argument
+        def clone_log(_, log_id, offset=0):
+            log_file = tmp_path / "test_log.txt"
+            log_file.write_bytes(b"test")
+            return str(log_file)
+        def is_healthy(_):
+            return True
+        def is_running(_):
+            return True
+        @property
+        def launches(_):
+            return 1
+        def log_length(_, log_id):
+            return 100
+    mon = _BasicMonitor()
+    test_log = mon.clone_log("test_log", offset=0)
+    assert os.path.isfile(test_log)
+    assert mon.is_healthy()
+    assert mon.is_running()
+    assert mon.launches == 1
+    assert mon.log_data("test_log") == b"test"
+    assert mon.log_length("test_log") == 100
