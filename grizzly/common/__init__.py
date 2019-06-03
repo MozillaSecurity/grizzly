7114a578b5910561f6a17e769320420857ebe755
Tyson Smith <tyson.w.smith@gmail.com>
2019-06-03 14:52:12 -0700

    Add AdapterError base class

--- grizzly/common/__init__.py
+++ grizzly/common/__init__.py
@@ -3,7 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from .adapter import Adapter
+from .adapter import Adapter, AdapterError
 from .iomanager import IOManager, ServerMap
 from .reduce_status import ReduceStatus
 from .reporter import FilesystemReporter, FuzzManagerReporter, Report, Reporter, S3FuzzManagerReporter
@@ -12,8 +12,8 @@ from .storage import InputFile, TestCase, TestFile
 
 
 __all__ = (
-    "Adapter", "FilesystemReporter", "FuzzManagerReporter", "IOManager", "InputFile",
-    "ReduceStatus", "Report", "Reporter", "S3FuzzManagerReporter", "ServerMap", "Status", "TestCase", "TestFile")
-
+    "Adapter", "AdapterError", "FilesystemReporter", "FuzzManagerReporter", "IOManager", "InputFile",
+    "ReduceStatus", "Report", "Reporter", "S3FuzzManagerReporter", "ServerMap", "Status", "TestCase",
+    "TestFile")
 __author__ = "Jesse Schwartzentruber"
 __credits__ = ["Jesse Schwartzentruber", "Tyson Smith"]
