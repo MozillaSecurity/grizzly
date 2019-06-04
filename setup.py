ed63afa39e1dd92ea4cfe02159e4580f230d7711
Tyson Smith <tyson.w.smith@gmail.com>
2019-06-04 14:29:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
         name='grizzly',
         packages=[
             'grizzly',
-            'grizzly.corpman',
+            'grizzly.common',
             'grizzly.reduce',
             'loki',
             'sapphire',
