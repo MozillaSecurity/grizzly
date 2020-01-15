afc15514f9d644c5256c38ddca832aca3d37833e
Tyson Smith <tyson.w.smith@gmail.com>
2020-01-15 09:09:57 -0800

    Move ServerMap to Sapphire

--- sapphire/__init__.py
+++ sapphire/__init__.py
@@ -7,7 +7,8 @@ Sapphire HTTP server
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from .core import Sapphire, SERVED_ALL, SERVED_NONE, SERVED_REQUEST, SERVED_TIMEOUT
+from .server_map import ServerMap
 
-__all__ = ("Sapphire", "SERVED_ALL", "SERVED_NONE", "SERVED_REQUEST", "SERVED_TIMEOUT")
+__all__ = ("Sapphire", "SERVED_ALL", "SERVED_NONE", "SERVED_REQUEST", "SERVED_TIMEOUT", "ServerMap")
 __author__ = "Tyson Smith"
 __credits__ = ["Tyson Smith"]
