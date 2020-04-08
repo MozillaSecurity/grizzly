7aab5346e4708276180b15837db2a74c6fa9b585
Tyson Smith <tyson.w.smith@gmail.com>
2020-04-08 14:37:27 -0700

    [tests] Cleanup IOManager tests

--- grizzly/common/test_iomanager.py
+++ grizzly/common/test_iomanager.py
@@ -4,16 +4,13 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # pylint: disable=protected-access
 
-import os
-
 from .iomanager import IOManager
 from .storage import InputFile, TestFile
 
 
 def test_iomanager_01():
     """test a simple IOManager"""
-    iom = IOManager()
-    try:
+    with IOManager() as iom:
         assert not iom.input_files
         assert iom.active_input is None
         assert iom.server_map is not None
@@ -22,13 +19,10 @@ def test_iomanager_01():
         assert iom._generated == 0
         assert iom._mime is None
         assert iom._report_size == 1
-    finally:
-        iom.cleanup()
 
 def test_iomanager_02(tmp_path):
     """test IOManager.scan_input()"""
-    iom = IOManager()
-    try:
+    with IOManager() as iom:
         # pass empty directory path
         iom.scan_input(str(tmp_path))
         assert not iom.input_files
@@ -53,13 +47,10 @@ def test_iomanager_02(tmp_path):
         iom.input_files = list()
         iom.scan_input(str(test_file))
         assert len(iom.input_files) == 1
-    finally:
-        iom.cleanup()
 
 def test_iomanager_03(tmp_path, mocker):
     """test IOManager._rotation_required()"""
-    iom = IOManager()
-    try:
+    with IOManager() as iom:
         # don't pick a file because we don't have inputs
         assert not iom._rotation_required(0)
         assert not iom._rotation_required(1)
@@ -91,50 +82,40 @@ def test_iomanager_03(tmp_path, mocker):
         iom._generated = 1
         iom.active_input = mocker.Mock(spec=InputFile)
         assert iom._rotation_required(0)
-    finally:
-        iom.cleanup()
 
 def test_iomanager_04():
-    """test IOManager.page_name() & IOManager.redirect_page()"""
-    iom = IOManager()
-    try:
+    """test IOManager.page_name()"""
+    with IOManager() as iom:
         assert iom.page_name() != iom.page_name(offset=1)
         next_page = iom.page_name(offset=1)
-        redirect_page = iom.redirect_page()
         iom._generated += 1
         assert iom.page_name() == next_page
-        assert iom.page_name() == redirect_page
-    finally:
-        iom.cleanup()
 
 def test_iomanager_05():
     """test IOManager.landing_page()"""
-    iom = IOManager()
-    try:
+    with IOManager() as iom:
         assert iom.landing_page() == iom.page_name()
         iom.harness = TestFile.from_data(b"data", "h.htm")
         assert iom.landing_page() == "h.htm"
-    finally:
-        iom.cleanup()
 
-def test_iomanager_06(tmp_path):
+def test_iomanager_06(mocker, tmp_path):
     """test IOManager._add_suppressions()"""
-    supp_file = tmp_path / "supp_file.txt"
-    supp_file.write_bytes(b"# test\n")
-    iom = IOManager()
-    try:
-        os.environ["ASAN_OPTIONS"] = "blah=1:suppressions='%s':foo=2" % (str(supp_file),)
+    fake_os = mocker.patch("grizzly.common.iomanager.os", autospec=True)
+    fake_os.environ = {}
+    with IOManager() as iom:
         assert not iom._environ_files
+        supp_file = tmp_path / "supp_file.txt"
+        supp_file.touch()
+        fake_os.environ = {
+            "ASAN_OPTIONS": "blah=1:suppressions='%s':foo=2" % (str(supp_file),),
+            "DEBUG": "1",
+            "JUNK": "test"}
         iom._add_suppressions()
         assert "asan.supp" in (x.file_name for x in iom._environ_files)
-    finally:
-        os.environ.pop("ASAN_OPTIONS", None)
-        iom.cleanup()
 
 def test_iomanager_07(tmp_path, mocker):
     """test IOManager.create_testcase()"""
-    iom = IOManager()
-    try:
+    with IOManager() as iom:
         assert iom._generated == 0
         assert iom._report_size == 1
         assert not iom.input_files
@@ -147,7 +128,7 @@ def test_iomanager_07(tmp_path, mocker):
         assert tcase is not None
         assert iom._generated == 1
         assert len(iom.tests) == 1
-        assert not list(tcase.optional)
+        assert not any(tcase.optional)
         # with a harness
         iom.harness = TestFile.from_data(b"data", "h.htm")
         tcase = iom.create_testcase("test-adapter")
@@ -173,34 +154,21 @@ def test_iomanager_07(tmp_path, mocker):
         assert tcase is not None
         assert iom._generated == 4
         assert iom.active_input.file_name == str(test_file)
-    finally:
-        iom.cleanup()
 
-def test_iomanager_08():
+def test_iomanager_08(mocker):
     """test IOManager.tracked_environ()"""
-    try:
-        org_tracked = IOManager.TRACKED_ENVVARS
-        IOManager.TRACKED_ENVVARS = ()
-        os.environ["ASAN_OPTIONS"] = "blah='z:/a':detect_leaks=1:foo=2"
-        os.environ["LSAN_OPTIONS"] = "detect_leaks='x:\\a.1':a=1"
-        os.environ["TEST_GOOD"] = "PASS"
-        os.environ["TEST_BAD"] = "FAIL"
-        assert not IOManager.tracked_environ()
-        IOManager.TRACKED_ENVVARS = ("ASAN_OPTIONS", "LSAN_OPTIONS", "TEST_GOOD", "TEST_MISSING")
-        tracked = IOManager.tracked_environ()
-        assert "TEST_BAD" not in tracked
-        assert "ASAN_OPTIONS" in tracked
-        assert tracked["ASAN_OPTIONS"] == "detect_leaks=1"
-        assert "LSAN_OPTIONS" in tracked
-        assert tracked["LSAN_OPTIONS"] == "detect_leaks='x:\\a.1'"
-        assert "TEST_GOOD" in tracked
-        assert tracked["TEST_GOOD"] == "PASS"
-        IOManager.TRACKED_ENVVARS = ("ASAN_OPTIONS",)
-        os.environ["ASAN_OPTIONS"] = "ignored=x"
-        assert not IOManager.tracked_environ()
-    finally:
-        IOManager.TRACKED_ENVVARS = org_tracked
-        os.environ.pop("ASAN_OPTIONS", None)
-        os.environ.pop("LSAN_OPTIONS", None)
-        os.environ.pop("TEST_GOOD", None)
-        os.environ.pop("TEST_BAD", None)
+    fake_os = mocker.patch("grizzly.common.iomanager.os", autospec=True)
+    fake_os.environ = {}
+    assert not IOManager.tracked_environ()
+    fake_os.environ = {
+        "ASAN_OPTIONS": "blah='z:/a':detect_leaks=1:foo=2",
+        "LSAN_OPTIONS": "detect_leaks='x:\\a.1':a=1",
+        "TEST_BAD": "FAIL"}
+    tracked = IOManager.tracked_environ()
+    assert "TEST_BAD" not in tracked
+    assert "ASAN_OPTIONS" in tracked
+    assert tracked["ASAN_OPTIONS"] == "detect_leaks=1"
+    assert "LSAN_OPTIONS" in tracked
+    assert tracked["LSAN_OPTIONS"] == "detect_leaks='x:\\a.1'"
+    fake_os.environ = {"ASAN_OPTIONS": "ignored=x"}
+    assert not IOManager.tracked_environ()
