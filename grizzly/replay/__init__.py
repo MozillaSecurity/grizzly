7875707a1447cf25f9f72f2385c79f4da219aa56
pyoor <pyoor@users.noreply.github.com>
2020-04-14 12:04:58 -0400

    Export ReplayArgs

--- grizzly/replay/__init__.py
+++ grizzly/replay/__init__.py
@@ -3,6 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-__all__ = ("ReplayManager",)
+__all__ = ("ReplayManager", "ReplayArgs")
 
 from .replay import ReplayManager
+from .args import ReplayArgs
