d5c4775ff9f3bd16ade5a42b746229198a8f3e39
Tyson Smith <tyson.w.smith@gmail.com>
2020-04-20 10:26:28 -0700

    Add Adapter.scan_path()

--- grizzly/common/test_adapter.py
+++ grizzly/common/test_adapter.py
@@ -2,8 +2,9 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import pytest
 
-from .adapter import Adapter
+from .adapter import Adapter, AdapterError
 
 
 class SimpleAdapter(Adapter):
@@ -14,6 +15,13 @@ class SimpleAdapter(Adapter):
 
 
 def test_adapter_01():
+    """test a bad Adapter"""
+    class BadAdapter(SimpleAdapter):
+        NAME = None
+    with pytest.raises(AdapterError, match="BadAdapter.NAME must be a string"):
+        BadAdapter()
+
+def test_adapter_02():
     """test a simple Adapter"""
     adpt = SimpleAdapter()
     assert isinstance(adpt.fuzz, dict)
@@ -28,7 +36,7 @@ def test_adapter_01():
     adpt.pre_launch()
     adpt.cleanup()
 
-def test_adapter_02(tmp_path):
+def test_adapter_03(tmp_path):
     """test Adapter.enable_harness()"""
     harness_file = tmp_path / "harness.html"
     adpt = SimpleAdapter()
@@ -53,3 +61,30 @@ def test_adapter_02(tmp_path):
     assert dump_harness.is_file()
     assert dump_harness.read_bytes() == test_data
     adpt.cleanup()
+
+def test_adapter_04(tmp_path):
+    """test Adapter.scan_path()"""
+    # empty path
+    assert not any(SimpleAdapter.scan_path(str(tmp_path)))
+    # missing path
+    assert not any(SimpleAdapter.scan_path(str(tmp_path / "none")))
+    # path to file
+    file1 = (tmp_path / "test1.txt")
+    file1.touch()
+    found = tuple(SimpleAdapter.scan_path(str(tmp_path)))
+    assert len(found) == 1
+    assert str(file1) in found
+    # path to directory
+    assert len(tuple(SimpleAdapter.scan_path(str(tmp_path)))) == 1
+    # path to directory (w/ ignored)
+    (tmp_path / ".ignored").touch()
+    nested = (tmp_path / "nested")
+    nested.mkdir()
+    file2 = (nested / "test2.bin")
+    file2.touch()
+    assert len(tuple(SimpleAdapter.scan_path(str(tmp_path)))) == 1
+    # path to directory (recursive)
+    found = tuple(SimpleAdapter.scan_path(str(tmp_path), recursive=True))
+    assert len(found) == 2
+    assert str(file1) in found
+    assert str(file2) in found
