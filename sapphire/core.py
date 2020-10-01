5ef110f4604b01b3ead3939c799aa7df7ef540e3
Tyson Smith <tyson.w.smith@gmail.com>
2020-10-01 15:43:09 -0700

    [sapphire] Update comments

--- sapphire/core.py
+++ sapphire/core.py
@@ -41,10 +41,21 @@ class Sapphire(object):
         self.close()
 
     @staticmethod
-    def _create_listening_socket(allow_remote, requested_port, retries=20):
-        # The intention of this function is to contain the socket creation code
-        # along with all the searching and retrying code. If a specific port is requested
-        # and it is not available a socket.error will be raised.
+    def _create_listening_socket(remote, port=None, retries=20):
+        """Create listening socket. Search for an open socket if needed and
+        and configure the socket. If a specific port is unavailable or no
+        available ports can be found a socket.error will be raised.
+
+        Args:
+            remote (bool): Accept all (non-local) incoming connections.
+            port (int): Port to listen on. If None is given a random port will
+                        be used.
+            retries (int): Number of attempts to the socket.
+
+        Returns:
+            socket: A listening socket.
+        """
+        addr = "0.0.0.0" if remote else "127.0.0.1"
         for retry in reversed(range(retries)):
             sock = None
             try:
@@ -53,8 +64,7 @@ class Sapphire(object):
                 sock.settimeout(0.25)
                 # find an unused port and avoid blocked ports
                 # see: dxr.mozilla.org/mozilla-central/source/netwerk/base/nsIOService.cpp
-                port = requested_port or randint(0x2000, 0xFFFF)
-                sock.bind(("0.0.0.0" if allow_remote else "127.0.0.1", port))
+                sock.bind((addr, port or randint(0x2000, 0xFFFF)))
                 sock.listen(5)
             except (OSError, sock_error) as soc_e:
                 if sock is not None:
@@ -67,38 +77,48 @@ class Sapphire(object):
         return sock
 
     def close(self):
-        """
-        close()
+        """Close listening server socket.
 
-        This function closes the listening server socket if it is open.
+        Args:
+            None
+
+        Returns:
+            None
         """
         if self._socket is not None:
             self._socket.close()
 
     @property
     def port(self):
-        """
-        port -> int
+        """Port number of listening socket.
 
-        returns the port number the socket is listening on
-        """
+        Args:
+            None
 
+        Returns:
+            int: Listening port number.
+        """
         return self._socket.getsockname()[1]
 
     def serve_path(self, path, continue_cb=None, forever=False, optional_files=None, server_map=None):
-        """
-        serve_path() -> tuple
-        path is the directory that will be used as wwwroot. The callback continue_cb should
-        be a function that returns True or False. If continue_cb is specified and returns False
-        the server serve loop will exit. optional_files is list of files that do not need to be
-        served in order to exit the serve loop.
+        """Serve files in path. On completion a list served files and a status
+        code will be returned.
+        The status codes include:
+            - SERVED_ALL: All files excluding files in optional_files were served
+            - SERVED_NONE: No files were served
+            - SERVED_REQUEST: Some files were requested
 
-        returns a tuple (server status, files served)
-        server status is an int:
-        - SERVED_ALL: All files excluding files int the optional_files list were served
-        - SERVED_NONE: No files were served
-        - SERVED_REQUEST: Some files were requested
-        files served is a list of the files that were served
+        Args:
+            path (str): Directory to use a wwwroot.
+            continue_cb (str): A callback that can be used to exit the serve loop.
+                               This should a function that returns a bool.
+            forever (bool): Continue to handle requests even after all files have
+                            been served. This is meant to be used with continue_cb.
+            optional_files (list): Files that do not need to be served in order
+                                   to exit the serve loop.
+            server_map (ServerMap):
+        Returns:
+            tuple: Files served and status code
         """
         LOG.debug("serving %r (forever=%r)", path, forever)
         job = Job(
@@ -118,10 +138,28 @@ class Sapphire(object):
 
     @property
     def timeout(self):
+        """The amount of time that must pass before exit the serve loop and
+        indicating a timeout.
+
+        Args:
+            None
+
+        Returns:
+            int: Timeout in seconds.
+        """
         return self._timeout
 
     @timeout.setter
     def timeout(self, value):
+        """The amount of time that must pass before exit the serve loop and
+        indicating a timeout.
+
+        Args:
+            value (int): Timeout in seconds.
+
+        Returns:
+            None
+        """
         if not value:
             self._timeout = 0
         else:
