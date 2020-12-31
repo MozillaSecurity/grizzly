717e206b0bb0eac8f729b66f242c8dd385256e7b
Tyson Smith <tyson.w.smith@gmail.com>
2020-12-30 18:00:23 -0800

    [loki] Add test coverage for arguments

--- loki/test_loki.py
+++ loki/test_loki.py
@@ -9,6 +9,7 @@ from struct import pack
 
 from pytest import raises
 
+from .args import parse_args
 from .loki import Loki
 
 
@@ -17,7 +18,6 @@ def test_loki_01(tmp_path):
     fuzzer = Loki(aggression=0.1)
     fuzzer.fuzz_file("nofile.test", 1, out_dir=str(tmp_path))
     assert not list(tmp_path.iterdir())
-
 
 def test_loki_02(tmp_path):
     """test an empty file"""
@@ -31,19 +31,24 @@ def test_loki_02(tmp_path):
     fuzzer.fuzz_file(str(tmp_fn), 1, out_dir=str(out_path))
     assert not list(out_path.iterdir())
 
-
-def test_loki_03(tmp_path):
+def test_loki_03(mocker, tmp_path):
     """test a single byte file"""
+    out_path = tmp_path / "out"
+    out_path.mkdir()
+    # no output path provided
+    fake_mkdtemp = mocker.patch(
+        "loki.loki.mkdtemp",
+        autospec=True,
+        return_value=str(out_path)
+    )
+
     in_data = b"A"
     tmp_fn = tmp_path / "input"
     tmp_fn.write_bytes(in_data)
 
-    out_path = tmp_path / "out"
-    out_path.mkdir()
-
     fuzzer = Loki(aggression=0.1)
     for _ in range(100):
-        fuzzer.fuzz_file(str(tmp_fn), 1, out_dir=str(out_path))
+        fuzzer.fuzz_file(str(tmp_fn), 1, out_dir=None)
         out_files = list(out_path.iterdir())
         assert len(out_files) == 1
         out_data = out_files[0].read_bytes()
@@ -51,7 +56,7 @@ def test_loki_03(tmp_path):
         if out_data != in_data:
             break
     assert out_data != in_data
-
+    assert fake_mkdtemp.call_count == 1
 
 def test_loki_04(tmp_path):
     """test a two byte file"""
@@ -73,7 +78,6 @@ def test_loki_04(tmp_path):
             break
     assert out_data != in_data
 
-
 def test_loki_05(tmp_path):
     """test a multi byte file"""
     in_size = 100
@@ -84,7 +88,6 @@ def test_loki_05(tmp_path):
     tmp_fn.write_bytes(in_data)
 
     out_path = tmp_path / "out"
-    out_path.mkdir()
 
     fuzzer = Loki(aggression=0.01)
     for _ in range(100):
@@ -102,7 +105,7 @@ def test_loki_05(tmp_path):
         if fuzz_found:
             break
     assert fuzz_found
-
+    assert out_path.is_dir()
 
 def test_loki_06():
     """test fuzz_data()"""
@@ -119,7 +122,6 @@ def test_loki_06():
             break
     assert fuzz_found
 
-
 def test_loki_07():
     """test invalid data sizes"""
     with raises(RuntimeError, match=r"Unsupported data size:"):
@@ -131,7 +133,6 @@ def test_loki_07():
     with raises(RuntimeError, match=r"Unsupported data size:"):
         Loki._fuzz_data(b"12345")
 
-
 def test_loki_08():
     """test endian support"""
     Loki._fuzz_data(b"1", ">")
@@ -139,13 +140,11 @@ def test_loki_08():
     with raises(RuntimeError, match=r"Unsupported byte order"):
         Loki._fuzz_data(b"1", "BAD")
 
-
 def test_loki_stress_01():
     """test with single byte"""
     for _ in range(1000):
         in_data = pack("B", getrandbits(8))
         assert len(Loki._fuzz_data(in_data)) == 1
-
 
 def test_loki_stress_02():
     """test with two bytes"""
@@ -153,9 +152,28 @@ def test_loki_stress_02():
     for _ in range(1000):
         assert len(Loki._fuzz_data(in_data)) == 2
 
-
 def test_loki_stress_03():
     """test with four bytes"""
     in_data = b"TEST"
     for _ in range(1000):
         assert len(Loki._fuzz_data(in_data)) == 4
+
+def test_main_01(mocker, tmp_path):
+    """test main"""
+    out_path = tmp_path / "out"
+    out_path.mkdir()
+
+    sample = tmp_path / "file.bin"
+    sample.write_bytes(b"test!")
+
+    args = mocker.Mock(
+        aggression=0.1,
+        count=15,
+        input=str(sample),
+        output=str(out_path)
+    )
+    Loki.main(args)
+
+def test_args_01():
+    """test parse_args()"""
+    assert parse_args(argv=["sample"])
