f8836f81eec3e68926e7d7908cdd0e4c15319251
Tyson Smith <tyson.w.smith@gmail.com>
2021-01-01 20:05:46 -0800

    [loki] Cleanups and move log output to main()

--- loki/loki.py
+++ loki/loki.py
@@ -1,10 +1,13 @@
-# coding: utf-8
+# coding=utf-8
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
 Loki fuzzing library
 """
 from logging import basicConfig, getLogger, ERROR, INFO
-from os import mkdir, SEEK_END
-from os.path import abspath, getsize, isdir, splitext, join as pathjoin
+from os import makedirs, SEEK_END
+from os.path import abspath, getsize, splitext, join as pathjoin
 from random import choice, getrandbits, randint
 from struct import pack, unpack
 import shutil
@@ -18,6 +21,8 @@ LOG = getLogger(__name__)
 
 
 class Loki(object):
+
+    __slots__ = ("aggr",)
 
     def __init__(self, aggression=0.0):
         self.aggr = min(max(aggression, 0.0), 1.0)
@@ -42,11 +47,11 @@ class Loki(object):
             byte_order = "<" if getrandbits(5) else ">"
         elif byte_order not in (">", "<"):
             raise RuntimeError("Unsupported byte order %r" % byte_order)
-        pack_unit = "".join([byte_order, pack_unit])
+        pack_unit = "%s%s" % (byte_order, pack_unit)
 
         fuzz_op = randint(0, 4)
         if fuzz_op == 0:  # boundary
-            out_data = (2 ** randint(2, (data_size * 8))) + randint(-2, 2)
+            out_data = (2 ** randint(2, data_size * 8)) + randint(-2, 2)
         elif fuzz_op == 1:  # arithmetic
             out_data = unpack(pack_unit, in_data)[0] + randint(-10, 10)
         elif fuzz_op == 2:  # interesting byte, short or int
@@ -55,19 +60,13 @@ class Loki(object):
             out_data = getrandbits(32)
         elif fuzz_op == 4 and data_size == 1:  # toggle
             out_data = unpack(pack_unit, in_data)[0] ^ (2 ** getrandbits(3))
-        elif fuzz_op == 4 and data_size == 2:  # multiple of a data size
-            if getrandbits(1):
+        elif fuzz_op == 4:  # multiple of 8
+            if data_size == 2:
                 out_data = randint(1, 0x1FFF) * 8
-            else:
-                out_data = randint(1, 0xFFF) * 16
-        elif fuzz_op == 4 and data_size == 4:  # multiple of a data size
-            fuzz_op = randint(0, 2)
-            if fuzz_op == 0:
+            elif data_size == 4:
                 out_data = randint(1, 0x1FFFFFFF) * 8
-            elif fuzz_op == 1:
-                out_data = randint(1, 0xFFFFFFF) * 16
-            elif fuzz_op == 2:
-                out_data = randint(1, 0x7FFFFFF) * 32
+            else:  # pragma: no cover
+                raise AssertionError("invalid data size")
 
         return pack(pack_unit, out_data & mask)
 
@@ -80,8 +79,11 @@ class Loki(object):
         # minimum number of max passes should be 1
         max_passes = max(int(round(length * self.aggr)), 1)
         fuzz_passes = randint(1, max_passes)
-        LOG.debug("%d of a possible %d fuzz passes will be performed", fuzz_passes, max_passes)
-
+        LOG.debug(
+            "%d of a possible %d fuzz passes will be performed",
+            fuzz_passes,
+            max_passes
+        )
         max_bytes = min(length, 2) if length < 4 else 4
         for _ in range(fuzz_passes):
             if max_bytes > 1 and not getrandbits(4):  # 6.25%
@@ -104,47 +106,23 @@ class Loki(object):
             tmp_fp.seek(0)
             return tmp_fp.read()
 
-    def fuzz_file(self, in_file, count, ext=None, out_dir=None):
-        start_time = time()
-        LOG.info("Starting Loki @ %s", strftime("%Y-%m-%d %H:%M:%S"))
-
-        # Analyze input test case
-        LOG.info("Target template is %r", abspath(in_file))
+    def fuzz_file(self, in_file, count, out_dir, ext=None):
         try:
-            length = getsize(in_file)
+            if getsize(in_file) < 1:
+                LOG.error("Input must be at least 1 byte long")
+                return False
         except OSError:
             LOG.error("%r does not exists!", in_file)
             return False
 
-        if length < 1:
-            LOG.error("Input must be at least 1 byte long")
-            return False
-
-        LOG.info("Template size in bytes is %d", length)
         if ext is None:
             ext = splitext(in_file)[1]
 
-        # Checking output directory
-        if out_dir is None:
-            out_dir = mkdtemp(
-                prefix=strftime("loki_%m%d%H%M_"),
-                dir=".")
-        elif not isdir(out_dir):
-            mkdir(out_dir)
-        LOG.info("Output directory is %s", abspath(out_dir))
-
-        LOG.info("Generating %s fuzzed test cases...", count)
         for i in range(count):
             out_file = pathjoin(out_dir, "".join(("%06d_fuzzed" % i, ext)))
             shutil.copy(in_file, out_file)
-
             with open(out_file, "r+b") as out_fp:
                 self._fuzz(out_fp)
-
-        finish_time = time() - start_time
-        LOG.info("Total run time %gs", finish_time)
-        if count > 0:
-            LOG.info("About %gs per file", finish_time / count)
 
         return True
 
@@ -176,9 +154,24 @@ class Loki(object):
     @classmethod
     def main(cls, args):
         basicConfig(format="", level=INFO if not args.quiet else ERROR)
-
+        LOG.info("Starting Loki @ %s", strftime("%Y-%m-%d %H:%M:%S"))
+        LOG.info("Target template is %r", abspath(args.input))
+        out_dir = args.output
+        if out_dir is None:
+            out_dir = mkdtemp(prefix=strftime("loki_%m-%d_%H-%M_"), dir=".")
+        makedirs(out_dir, exist_ok=True)
+        LOG.info("Output directory is %r", abspath(out_dir))
+        count = max(args.count, 1)
+        LOG.info("Generating %d fuzzed test cases...", count)
         loki = Loki(aggression=args.aggression)
         try:
-            loki.fuzz_file(args.input, args.count, out_dir=args.output)
+            start_time = time()
+            success = loki.fuzz_file(args.input, count, out_dir)
+            finish_time = time() - start_time
+            LOG.info("Done. Total run time %gs", finish_time)
+            if success:
+                LOG.info("About %gs per file", finish_time / count)
         except KeyboardInterrupt:  # pragma: no cover
             LOG.info("Ctrl+C detected.")
+            success = False
+        return 0 if success else 1
--- loki/test_loki.py
+++ loki/test_loki.py
@@ -4,8 +4,9 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # pylint: disable=protected-access
 from os import SEEK_END
-from random import getrandbits
+from random import choice, getrandbits
 from struct import pack
+from tempfile import SpooledTemporaryFile
 
 from pytest import raises
 
@@ -16,7 +17,7 @@ from .loki import Loki
 def test_loki_01(tmp_path):
     """test a missing file"""
     fuzzer = Loki(aggression=0.1)
-    fuzzer.fuzz_file("nofile.test", 1, out_dir=str(tmp_path))
+    assert not fuzzer.fuzz_file("nofile.test", 1, str(tmp_path))
     assert not list(tmp_path.iterdir())
 
 def test_loki_02(tmp_path):
@@ -28,19 +29,13 @@ def test_loki_02(tmp_path):
     out_path.mkdir()
 
     fuzzer = Loki(aggression=0.1)
-    fuzzer.fuzz_file(str(tmp_fn), 1, out_dir=str(out_path))
+    assert not fuzzer.fuzz_file(str(tmp_fn), 1, str(out_path))
     assert not list(out_path.iterdir())
 
-def test_loki_03(mocker, tmp_path):
+def test_loki_03(tmp_path):
     """test a single byte file"""
     out_path = tmp_path / "out"
     out_path.mkdir()
-    # no output path provided
-    fake_mkdtemp = mocker.patch(
-        "loki.loki.mkdtemp",
-        autospec=True,
-        return_value=str(out_path)
-    )
 
     in_data = b"A"
     tmp_fn = tmp_path / "input"
@@ -48,7 +43,7 @@ def test_loki_03(mocker, tmp_path):
 
     fuzzer = Loki(aggression=0.1)
     for _ in range(100):
-        fuzzer.fuzz_file(str(tmp_fn), 1, out_dir=None)
+        assert fuzzer.fuzz_file(str(tmp_fn), 1, str(out_path))
         out_files = list(out_path.iterdir())
         assert len(out_files) == 1
         out_data = out_files[0].read_bytes()
@@ -56,7 +51,6 @@ def test_loki_03(mocker, tmp_path):
         if out_data != in_data:
             break
     assert out_data != in_data
-    assert fake_mkdtemp.call_count == 1
 
 def test_loki_04(tmp_path):
     """test a two byte file"""
@@ -69,7 +63,7 @@ def test_loki_04(tmp_path):
 
     fuzzer = Loki(aggression=0.1)
     for _ in range(100):
-        fuzzer.fuzz_file(str(tmp_fn), 1, out_dir=str(out_path))
+        assert fuzzer.fuzz_file(str(tmp_fn), 1, str(out_path))
         out_files = list(out_path.iterdir())
         assert len(out_files) == 1
         out_data = out_files[0].read_bytes()
@@ -88,10 +82,11 @@ def test_loki_05(tmp_path):
     tmp_fn.write_bytes(in_data)
 
     out_path = tmp_path / "out"
+    out_path.mkdir()
 
     fuzzer = Loki(aggression=0.01)
     for _ in range(100):
-        fuzzer.fuzz_file(str(tmp_fn), 1, out_dir=str(out_path))
+        assert fuzzer.fuzz_file(str(tmp_fn), 1, str(out_path))
         out_files = list(out_path.iterdir())
         assert len(out_files) == 1
         with out_files[0].open("rb") as out_fp:
@@ -105,7 +100,6 @@ def test_loki_05(tmp_path):
         if fuzz_found:
             break
     assert fuzz_found
-    assert out_path.is_dir()
 
 def test_loki_06():
     """test fuzz_data()"""
@@ -140,39 +134,80 @@ def test_loki_08():
     with raises(RuntimeError, match=r"Unsupported byte order"):
         Loki._fuzz_data(b"1", "BAD")
 
+def test_loki_fuzz_01(mocker):
+    """test _fuzz() paths"""
+    mocker.patch("loki.loki.randint", autospec=True, side_effect=max)
+    loki = Loki(aggression=1)
+    with SpooledTemporaryFile(max_size=0x800000, mode="r+b") as tmp_fp:
+        # test empty input sample
+        with raises(RuntimeError):
+            loki._fuzz(tmp_fp)
+        tmp_fp.write(b"123456789")
+        # try getrandbits returning 0
+        mocker.patch("loki.loki.getrandbits", autospec=True, return_value=0)
+        loki._fuzz(tmp_fp)
+        # try getrandbits returning 1
+        mocker.patch("loki.loki.getrandbits", autospec=True, return_value=1)
+        loki._fuzz(tmp_fp)
+
+def test_loki_fuzz_02(mocker):
+    """test Loki._fuzz_data() paths"""
+    fake_randint = mocker.patch("loki.loki.randint", autospec=True)
+    # fuzz op 0
+    fake_randint.side_effect = (0, 1, 1)
+    Loki._fuzz_data(b"1")
+    # fuzz op 1
+    fake_randint.side_effect = (1, 0)
+    Loki._fuzz_data(b"1")
+    # fuzz op 2
+    fake_randint.side_effect = (2,)
+    Loki._fuzz_data(b"1")
+    # fuzz op 3
+    fake_randint.side_effect = (3,)
+    Loki._fuzz_data(b"1")
+    # fuzz op 4 & test data size 1
+    fake_randint.side_effect = max
+    Loki._fuzz_data(b"1")
+    # fuzz op 4 & test data size 2
+    Loki._fuzz_data(b"12")
+    # fuzz op 4 & test data size 4
+    fake_randint.side_effect = (4, 1)
+    Loki._fuzz_data(b"1234")
+
 def test_loki_stress_01():
-    """test with single byte"""
-    for _ in range(1000):
-        in_data = pack("B", getrandbits(8))
-        assert len(Loki._fuzz_data(in_data)) == 1
-
-def test_loki_stress_02():
-    """test with two bytes"""
-    in_data = b"\xff\xff"
-    for _ in range(1000):
-        assert len(Loki._fuzz_data(in_data)) == 2
-
-def test_loki_stress_03():
-    """test with four bytes"""
-    in_data = b"TEST"
-    for _ in range(1000):
-        assert len(Loki._fuzz_data(in_data)) == 4
+    """test Loki._fuzz_data() with random input"""
+    orders = ("<", ">", None)
+    sizes = (1, 2, 4)
+    for _ in range(5000):
+        size = choice(sizes)
+        if size == 1:
+            in_data = pack("B", getrandbits(8))
+        elif size == 2:
+            in_data = pack("H", getrandbits(16))
+        elif size == 4:
+            in_data = pack("I", getrandbits(32))
+        assert len(Loki._fuzz_data(in_data, byte_order=choice(orders))) == size
 
 def test_main_01(mocker, tmp_path):
     """test main"""
     out_path = tmp_path / "out"
     out_path.mkdir()
-
+    # no output path provided
+    fake_mkdtemp = mocker.patch(
+        "loki.loki.mkdtemp",
+        autospec=True,
+        return_value=str(out_path)
+    )
     sample = tmp_path / "file.bin"
     sample.write_bytes(b"test!")
-
     args = mocker.Mock(
         aggression=0.1,
         count=15,
         input=str(sample),
-        output=str(out_path)
+        output=None
     )
-    Loki.main(args)
+    assert Loki.main(args) == 0
+    assert fake_mkdtemp.call_count == 1
 
 def test_args_01():
     """test parse_args()"""
