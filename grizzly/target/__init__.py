a64fc17aa297c361c636f255d4200d174d0a7fee
Tyson Smith <tyson.w.smith@gmail.com>
2021-07-19 13:34:47 -0700

    Move AssetManager to assets.py

--- grizzly/target/__init__.py
+++ grizzly/target/__init__.py
@@ -2,14 +2,8 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from .target import (
-    AssetError,
-    AssetManager,
-    Target,
-    TargetError,
-    TargetLaunchError,
-    TargetLaunchTimeout,
-)
+from .assets import AssetError, AssetManager
+from .target import Target, TargetError, TargetLaunchError, TargetLaunchTimeout
 
 __all__ = (
     "AssetError",
