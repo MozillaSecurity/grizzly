90662d79f0a8a814461a9fa01916768b7b6bc57e
Tyson Smith <tyson.w.smith@gmail.com>
2021-11-18 16:50:18 -0800

    [sapphire] Simplify and rename check_request() -> lookup_resource()

--- sapphire/test_job.py
+++ sapphire/test_job.py
@@ -2,12 +2,11 @@
 """
 Job unit tests
 """
-# pylint: disable=protected-access
 
-import platform
 from pathlib import Path
+from platform import system
 
-import pytest
+from pytest import mark, raises
 
 from .job import Job, Served
 from .server_map import Resource, ServerMap
@@ -18,10 +17,11 @@ def test_job_01(tmp_path):
     job = Job(str(tmp_path))
     assert not job.forever
     assert job.status == Served.ALL
-    assert job.check_request("") is None
-    assert job.check_request("test") is None
-    assert job.check_request("test/test/") is None
-    assert job.check_request("test/../../") is None
+    assert job.lookup_resource("") is None
+    assert job.lookup_resource("test") is None
+    assert job.lookup_resource("test/test/") is None
+    assert job.lookup_resource("test/../../") is None
+    assert job.lookup_resource("\x00\x0B\xAD\xF0\x0D") is None
     assert not job.is_forbidden(str(tmp_path))
     assert not job.is_forbidden(str(tmp_path / "missing_file"))
     assert job.pending == 0
@@ -48,10 +48,10 @@ def test_job_02(tmp_path):
     )
     assert job.status == Served.NONE
     assert not job.is_complete()
-    resource = job.check_request("req_file_1.txt")
+    resource = job.lookup_resource("req_file_1.txt")
     assert resource.required
     assert job.pending == 2
-    assert resource.target == str(tmp_path / "req_file_1.txt")
+    assert resource.target == tmp_path / "req_file_1.txt"
     assert resource.type == Resource.URL_FILE
     assert not job.is_forbidden(str(req1_path))
     assert not job.remove_pending("no_file.test")
@@ -63,13 +63,13 @@ def test_job_02(tmp_path):
     assert job.status == Served.ALL
     assert job.pending == 0
     assert job.remove_pending(str(req1_path))
-    resource = job.check_request("opt_file_1.txt")
+    resource = job.lookup_resource("opt_file_1.txt")
     assert not resource.required
-    assert resource.target == str(opt1_path)
+    assert resource.target == opt1_path
     assert resource.type == Resource.URL_FILE
     assert job.remove_pending(str(opt1_path))
-    resource = job.check_request("nested/opt_file_2.txt")
-    assert resource.target == str(opt2_path)
+    resource = job.lookup_resource("nested/opt_file_2.txt")
+    assert resource.target == opt2_path
     assert resource.type == Resource.URL_FILE
     assert not resource.required
     job.finish()
@@ -83,9 +83,9 @@ def test_job_03(tmp_path):
     smap.set_redirect("two", "reqfile.txt")
     job = Job(str(tmp_path), server_map=smap)
     assert job.status == Served.NONE
-    resource = job.check_request("one")
+    resource = job.lookup_resource("one")
     assert resource.type == Resource.URL_REDIRECT
-    resource = job.check_request("two")
+    resource = job.lookup_resource("two")
     assert resource is not None
     assert resource.type == Resource.URL_REDIRECT
     assert job.pending == 1
@@ -117,37 +117,39 @@ def test_job_04(mocker, tmp_path):
     smap = ServerMap()
     smap.set_include("testinc", str(srv_include))
     # add manually to avoid sanity checks in ServerMap.set_include()
-    smap.include["testinc/fakedir"] = Resource(Resource.URL_INCLUDE, str(srv_include))
-    smap.include["testinc/1/2/3"] = Resource(Resource.URL_INCLUDE, str(srv_include))
-    smap.include[""] = Resource(Resource.URL_INCLUDE, str(srv_include))
+    smap.include["testinc/fakedir"] = Resource(Resource.URL_INCLUDE, srv_include)
+    smap.include["testinc/1/2/3"] = Resource(Resource.URL_INCLUDE, srv_include)
+    smap.include[""] = Resource(Resource.URL_INCLUDE, srv_include)
     smap.set_include("testinc/inc2", str(srv_include_2))
     job = Job(str(srv_root), server_map=smap)
     assert job.status == Served.NONE
+    # test include path pointing to a missing file
+    assert job.lookup_resource("testinc/missing") is None
     # test includes that map to 'srv_include'
     for incl, inc_path in smap.include.items():
         if inc_path != str(srv_include):  # only check 'srv_include' mappings
             continue
-        resource = job.check_request("/".join([incl, "test_file.txt"]))
+        resource = job.lookup_resource("/".join([incl, "test_file.txt"]))
         assert resource.type == Resource.URL_INCLUDE
-        assert resource.target == str(inc_1)
+        assert resource.target == inc_1
     # test nested include path pointing to a different include
-    resource = job.check_request("testinc/inc2/test_file_2.txt")
+    resource = job.lookup_resource("testinc/inc2/test_file_2.txt")
     assert resource.type == Resource.URL_INCLUDE
-    assert resource.target == str(inc_2)
+    assert resource.target == inc_2
     # test redirect root without leading '/'
-    resource = job.check_request("test_file.txt")
+    resource = job.lookup_resource("test_file.txt")
     assert resource.type == Resource.URL_INCLUDE
-    assert resource.target == str(srv_include / "test_file.txt")
+    assert resource.target == srv_include / "test_file.txt"
     # test redirect with file in a nested directory
-    resource = job.check_request("/".join(["testinc", "nested", "nested_file.txt"]))
+    resource = job.lookup_resource("/".join(["testinc", "nested", "nested_file.txt"]))
     assert resource.type == Resource.URL_INCLUDE
-    assert resource.target == str(nst_1)
+    assert resource.target == nst_1
     assert not job.is_forbidden(str(srv_root / ".." / "test" / "test_file.txt"))
     assert not job.is_forbidden(str(srv_include / ".." / "root" / "req_file.txt"))
 
 
 def test_job_05(tmp_path):
-    """test Job.check_request() with tricky includes"""
+    """test Job.lookup_resource() with tricky includes"""
     srv_root = tmp_path / "root"
     srv_root.mkdir()
     req = srv_root / "req_file.txt"
@@ -163,11 +165,11 @@ def test_job_05(tmp_path):
     smap = ServerMap()
     smap.include["inc"] = Resource(Resource.URL_INCLUDE, str(inc_dir))
     job = Job(str(srv_root), server_map=smap)
-    resource = job.check_request("inc/sub/include.js")
+    resource = job.lookup_resource("inc/sub/include.js")
     assert resource.type == Resource.URL_INCLUDE
-    assert resource.target == str(inc_file1)
+    assert resource.target == inc_file1
     # test checking only the include url
-    assert job.check_request("inc") is None
+    assert job.lookup_resource("inc") is None
     # file and include file collision (files should always win)
     smap.include.clear()
     inc_a = inc_dir / "a.bin"
@@ -175,22 +177,13 @@ def test_job_05(tmp_path):
     file_a = srv_root / "a.bin"
     file_a.write_bytes(b"a")
     smap.include["/"] = Resource(Resource.URL_INCLUDE, str(inc_dir))
-    resource = job.check_request("a.bin")
+    resource = job.lookup_resource("a.bin")
     assert resource.type == Resource.URL_FILE
-    assert resource.target == str(file_a)
-    # inc and inc subdir collision
-    # TODO: This can fail. How do we detect or support it?
-    # smap.include.clear()
-    # (inc_dir / "c").mkdir()
-    # inc_c_d = (inc_dir / "c" / "d.bin")
-    # inc_c_d.write_bytes(b"a")
-    # inc_d = (inc_dir / "d.bin")
-    # inc_d.write_bytes(b"a")
-    # smap.include["c"] = Resource(Resource.URL_INCLUDE, str(inc_dir))
-    # smap.include[""] = Resource(Resource.URL_INCLUDE, str(inc_dir / "c"))
-    # resource = job.check_request("c/d.bin")
-    # assert resource.type == Resource.URL_INCLUDE
-    # assert resource.target == str(inc_c_d)
+    assert resource.target == file_a
+    # TODO: inc and inc subdir collision can fail.
+    # /inc/a file
+    # /inc   a/file
+    # Should we detect and support it? Does it matter?
 
 
 def test_job_06(tmp_path):
@@ -201,11 +194,11 @@ def test_job_06(tmp_path):
     job = Job(str(tmp_path), server_map=smap)
     assert job.status == Served.ALL
     assert job.pending == 0
-    resource = job.check_request("cb1")
+    resource = job.lookup_resource("cb1")
     assert resource.type == Resource.URL_DYNAMIC
     assert callable(resource.target)
     assert isinstance(resource.mime, str)
-    resource = job.check_request("cb2")
+    resource = job.lookup_resource("cb2")
     assert resource is not None
     assert resource.type == Resource.URL_DYNAMIC
     assert callable(resource.target)
@@ -223,14 +216,14 @@ def test_job_07(tmp_path):
     job = Job(str(srv_root))
     assert job.status == Served.NONE
     assert job.pending == 1
-    resource = job.check_request("../no_access.txt")
-    assert resource.target == str(no_access)
+    resource = job.lookup_resource("../no_access.txt")
+    assert resource.target == no_access
     assert resource.type == Resource.URL_FILE
     assert not job.is_forbidden(str(test_1))
-    assert job.is_forbidden(str(srv_root / "../no_access.txt"))
+    assert job.is_forbidden(str(srv_root / ".." / "no_access.txt"))
 
 
-@pytest.mark.skipif(platform.system() == "Windows", reason="Unsupported on Windows")
+@mark.skipif(system() == "Windows", reason="Unsupported on Windows")
 def test_job_08(tmp_path):
     """test Job with file names containing invalid characters"""
     test_file = tmp_path / "test.txt"
@@ -239,12 +232,12 @@ def test_job_08(tmp_path):
     job = Job(str(tmp_path))
     assert job.status == Served.NONE
     assert job.pending == 1
-    assert job.check_request("test.txt").target == str(test_file)
+    assert job.lookup_resource("test.txt").target == test_file
 
 
 def test_job_09():
     """test Job with missing directory"""
-    with pytest.raises(OSError):
+    with raises(OSError):
         Job("missing")
 
 
