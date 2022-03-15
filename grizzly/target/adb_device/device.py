f6765253573cdc3bc0a0de6985a62f5a4e6fd48d
Tyson Smith <tyson.w.smith@gmail.com>
2022-03-15 10:49:39 -0700

    [adb] Split main() and parse_args() into separate functions

--- grizzly/target/adb_device/device.py
+++ grizzly/target/adb_device/device.py
@@ -1,7 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from argparse import ArgumentParser
-from logging import DEBUG, INFO, basicConfig, getLogger
-from os import getenv
+from logging import DEBUG, ERROR, INFO, WARNING, getLogger
 from os.path import isfile
+
+from grizzly.common.utils import configure_logging
 
 from .adb_process import ADBProcess
 from .adb_session import ADBSession
@@ -12,25 +16,24 @@ __author__ = "Tyson Smith"
 __credits__ = ["Tyson Smith"]
 
 
-def main(argv=None):  # pylint: disable=missing-docstring
-    # set output verbosity
-    if bool(getenv("DEBUG")):
-        log_level = DEBUG
-        log_fmt = "%(levelname).1s %(name)s [%(asctime)s] %(message)s"
-    else:
-        log_level = INFO
-        log_fmt = "[%(asctime)s] %(message)s"
-    basicConfig(format=log_fmt, datefmt="%Y-%m-%d %H:%M:%S", level=log_level)
+def parse_args(argv=None):
+    log_level_map = {"ERROR": ERROR, "WARN": WARNING, "INFO": INFO, "DEBUG": DEBUG}
 
     parser = ArgumentParser(description="ADB Device Wrapper")
     parser.add_argument(
         "--airplane-mode",
-        default=None,
+        choices=(0, 1),
         type=int,
         help="Enable(1) or disable(0) airplane mode",
     )
     parser.add_argument("--install", help="Path to APK to install")
     parser.add_argument("--launch", help="Path to APK to launch")
+    parser.add_argument(
+        "--log-level",
+        choices=sorted(log_level_map),
+        default="INFO",
+        help="Configure console logging (default: %(default)s)",
+    )
     parser.add_argument("--logs", help="Location to save logs")
     parser.add_argument("--ip", help="IP address of target device")
     parser.add_argument(
@@ -40,16 +43,20 @@ def main(argv=None):  # pylint: disable=missing-docstring
         "--port", default=5555, type=int, help="ADB listening port on target device"
     )
     parser.add_argument("--prep", help="Prepare the device for fuzzing. Path to APK")
+
+    # sanity check args
     args = parser.parse_args(argv)
     if not any((args.airplane_mode is not None, args.install, args.launch, args.prep)):
         parser.error("No options selected")
-
-    # sanity check the APK
     for apk in (args.install, args.launch, args.prep):
         if apk is not None and not isfile(apk):
-            LOG.error("Invalid APK %r", apk)
-            return 1
+            parser.error("Invalid APK %r" % (apk,))
+    args.log_level = log_level_map[args.log_level]
+    return args
 
+
+def main(args):  # pylint: disable=missing-docstring
+    configure_logging(args.log_level)
     LOG.debug("opening a session")
     session = ADBSession.create(args.ip, args.port, as_root=not args.non_root)
     if session is None:
@@ -62,23 +69,23 @@ def main(argv=None):  # pylint: disable=missing-docstring
             args.install = args.prep
             session.sanitizer_options("asan", {"abort_on_error": "0"})
         if args.airplane_mode is not None:
+            LOG.debug("Setting airplane mode (%d)...", args.airplane_mode)
+            session.airplane_mode = args.airplane_mode == 1
             if args.airplane_mode > 0:
-                LOG.info("Enabling airplane mode...")
+                LOG.info("Enabled airplane mode.")
             else:
-                LOG.info("Disabling airplane mode...")
-            session.airplane_mode = bool(args.airplane_mode)
-            LOG.info("Done.")
+                LOG.info("Disabled airplane mode.")
         if args.install is not None:
             pkg_name = ADBSession.get_package_name(args.install)
             if pkg_name is None:
                 LOG.error("Failed to lookup package name in %r", args.install)
                 return 1
             if session.uninstall(pkg_name):
-                LOG.info("Uninstalled existing version")
-            LOG.info("Installing %s (%r)...", pkg_name, args.install)
+                LOG.info("Uninstalled existing %r.", pkg_name)
+            LOG.info("Installing %r from %r...", pkg_name, args.install)
             package = session.install(args.install)
             if not package:
-                LOG.error("Could not install %s", args.install)
+                LOG.error("Could not install %r", args.install)
                 return 1
             session.call(["shell", "am", "set-debug-app", "--persistent", package])
             LOG.info("Installed %s.", package)
@@ -107,4 +114,4 @@ def main(argv=None):  # pylint: disable=missing-docstring
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    raise SystemExit(main(parse_args()))
