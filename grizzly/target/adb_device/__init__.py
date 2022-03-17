a4b04db7d5d59d0561fca6490256b990ced47b1e
Tyson Smith <tyson.w.smith@gmail.com>
2022-03-17 10:36:32 -0700

    [adb] Add missing header comments

--- grizzly/target/adb_device/__init__.py
+++ grizzly/target/adb_device/__init__.py
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from .adb_process import ADBProcess, Reason
 from .adb_session import ADBSession
 
