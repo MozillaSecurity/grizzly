ec4eff82d8647937653bf164eb8ca03416e21d9b
Tyson Smith <tyson.w.smith@gmail.com>
2023-09-27 11:11:04 -0700

    [sapphire] Remove launcher thread
    Working to reduce complexity and avoid potential deadlocks.
    Also increase code coverage and adds docs.

--- sapphire/test_connection_manager.py
+++ sapphire/test_connection_manager.py
@@ -5,15 +5,16 @@ ConnectionManager unit tests
 
 from itertools import count
 from socket import socket
-from threading import ThreadError
 
-from pytest import raises
+from pytest import mark, raises
 
 from .connection_manager import ConnectionManager
 from .job import Job
+from .worker import Worker
 
 
-def test_connection_manager_01(mocker, tmp_path):
+@mark.parametrize("timeout", [10, 0])
+def test_connection_manager_01(mocker, tmp_path, timeout):
     """test basic ConnectionManager"""
     (tmp_path / "testfile").write_bytes(b"test")
     job = Job(tmp_path)
@@ -22,29 +23,16 @@ def test_connection_manager_01(mocker, tmp_path):
     serv_sock = mocker.Mock(spec_set=socket)
     serv_sock.accept.return_value = (clnt_sock, None)
     assert not job.is_complete()
-    with ConnectionManager(job, serv_sock) as loadmgr:
-        assert loadmgr.wait(1)
+    with ConnectionManager(job, serv_sock) as mgr:
+        assert mgr.serve(timeout)
     assert clnt_sock.close.call_count == 1
     assert job.is_complete()
     assert not job.accepting.is_set()
     assert job.exceptions.empty()
 
 
-def test_connection_manager_02(mocker):
-    """test ConnectionManager.start() failure"""
-    mocker.patch("sapphire.connection_manager.sleep", autospec=True)
-    fake_thread = mocker.patch("sapphire.connection_manager.Thread", autospec=True)
-    fake_thread.return_value.start.side_effect = ThreadError
-    job = mocker.Mock(spec_set=Job)
-    job.pending = True
-    loadmgr = ConnectionManager(job, None)
-    with raises(ThreadError):
-        loadmgr.start()
-    loadmgr.close()
-    assert job.is_complete()
-
-
-def test_connection_manager_03(mocker, tmp_path):
+@mark.parametrize("worker_limit", [1, 2, 10])
+def test_connection_manager_02(mocker, tmp_path, worker_limit):
     """test ConnectionManager multiple files and requests"""
     (tmp_path / "test1").touch()
     (tmp_path / "test2").touch()
@@ -64,35 +52,13 @@ def test_connection_manager_03(mocker, tmp_path):
     serv_sock = mocker.Mock(spec_set=socket)
     serv_sock.accept.return_value = (clnt_sock, None)
     assert not job.is_complete()
-    with ConnectionManager(job, serv_sock, max_workers=2) as loadmgr:
-        assert loadmgr.wait(1)
+    with ConnectionManager(job, serv_sock, limit=worker_limit) as mgr:
+        assert mgr.serve(10)
     assert clnt_sock.close.call_count == 8
     assert job.is_complete()
 
 
-def test_connection_manager_04(mocker, tmp_path):
-    """test ConnectionManager.wait()"""
-    (tmp_path / "test1").touch()
-    job = Job(tmp_path)
-    clnt_sock = mocker.Mock(spec_set=socket)
-    clnt_sock.recv.return_value = b""
-    serv_sock = mocker.Mock(spec_set=socket)
-    serv_sock.accept.return_value = (clnt_sock, None)
-    with ConnectionManager(job, serv_sock, max_workers=10) as loadmgr:
-        # invalid callback
-        with raises(TypeError, match="continue_cb must be callable"):
-            loadmgr.wait(0, continue_cb="test")
-        # callback abort
-        assert loadmgr.wait(1, continue_cb=lambda: False, poll=0.01)
-    # timeout
-    job = Job(tmp_path)
-    fake_time = mocker.patch("sapphire.connection_manager.time", autospec=True)
-    fake_time.side_effect = count()
-    with ConnectionManager(job, serv_sock, max_workers=10) as loadmgr:
-        assert not loadmgr.wait(1, continue_cb=lambda: True, poll=0.01)
-
-
-def test_connection_manager_05(mocker, tmp_path):
+def test_connection_manager_03(mocker, tmp_path):
     """test ConnectionManager re-raise worker exceptions"""
     (tmp_path / "test1").touch()
     job = Job(tmp_path)
@@ -101,21 +67,75 @@ def test_connection_manager_05(mocker, tmp_path):
     serv_sock = mocker.Mock(spec_set=socket)
     serv_sock.accept.return_value = (clnt_sock, None)
     with raises(Exception, match="worker exception"):
-        with ConnectionManager(job, serv_sock) as loadmgr:
-            loadmgr.wait(1)
+        with ConnectionManager(job, serv_sock) as mgr:
+            mgr.serve(10)
     assert clnt_sock.close.call_count == 1
     assert job.is_complete()
     assert job.exceptions.empty()
 
 
-def test_connection_manager_06(mocker, tmp_path):
-    """test ConnectionManager re-raise launcher exceptions"""
+def test_connection_manager_04(mocker, tmp_path):
+    """test ConnectionManager.serve() with callback"""
     (tmp_path / "test1").touch()
     job = Job(tmp_path)
+    with ConnectionManager(job, mocker.Mock(spec_set=socket), poll=0.01) as mgr:
+        # invalid callback
+        with raises(TypeError, match="continue_cb must be callable"):
+            mgr.serve(10, continue_cb="test")
+        # job did not start
+        assert not job.is_complete()
+        # callback abort
+        assert mgr.serve(10, continue_cb=lambda: False)
+        assert job.is_complete()
+
+
+def test_connection_manager_05(mocker, tmp_path):
+    """test ConnectionManager.serve() with timeout"""
+    mocker.patch("sapphire.connection_manager.time", autospec=True, side_effect=count())
+    (tmp_path / "test1").touch()
+    job = Job(tmp_path)
+    clnt_sock = mocker.Mock(spec_set=socket)
+    clnt_sock.recv.return_value = b""
     serv_sock = mocker.Mock(spec_set=socket)
-    serv_sock.accept.side_effect = Exception("launcher exception")
-    with raises(Exception, match="launcher exception"):
-        with ConnectionManager(job, serv_sock) as loadmgr:
-            loadmgr.wait(1)
-    assert job.is_complete()
-    assert job.exceptions.empty()
+    serv_sock.accept.return_value = (clnt_sock, None)
+    job = Job(tmp_path)
+    with ConnectionManager(job, serv_sock, poll=0.01) as mgr:
+        assert not mgr.serve(10)
+        assert job.is_complete()
+
+
+def test_connection_manager_06(mocker, tmp_path):
+    """test ConnectionManager.serve() worker fails to exit"""
+    mocker.patch("sapphire.worker.Thread", autospec=True)
+    mocker.patch("sapphire.connection_manager.time", autospec=True, side_effect=count())
+    (tmp_path / "test1").touch()
+    clnt_sock = mocker.Mock(spec_set=socket)
+    serv_sock = mocker.Mock(spec_set=socket)
+    serv_sock.accept.return_value = (clnt_sock, None)
+    job = Job(tmp_path)
+    mocker.patch.object(job, "worker_complete")
+    with ConnectionManager(job, serv_sock) as mgr:
+        with raises(RuntimeError, match="Failed to close workers"):
+            mgr.serve(10)
+        assert job.is_complete()
+    assert clnt_sock.close.call_count == 1
+
+
+def test_connection_manager_07(mocker):
+    """test ConnectionManager._join_workers()"""
+    # no workers
+    assert not ConnectionManager._join_workers([])
+    # worker fails to join, without timeout
+    fake_worker = mocker.Mock(spec_set=Worker)
+    fake_worker.join.return_value = False
+    assert ConnectionManager._join_workers([fake_worker], timeout=0)
+    assert fake_worker.join.call_count == 1
+    fake_worker.reset_mock()
+    # worker fails to join, with timeout
+    assert ConnectionManager._join_workers([fake_worker], timeout=1)
+    assert fake_worker.join.call_count == 1
+    fake_worker.reset_mock()
+    # worker joins
+    fake_worker.join.return_value = True
+    assert not ConnectionManager._join_workers([fake_worker], timeout=0)
+    assert fake_worker.join.call_count == 1
