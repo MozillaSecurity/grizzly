7e786950502ac7c25437a9517fef042c8f802504
Tyson Smith <tyson.w.smith@gmail.com>
2025-02-18 16:09:49 -0800

    feat: add interprocess_lock

--- src/grizzly/common/test_utils.py
+++ src/grizzly/common/test_utils.py
@@ -2,10 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from importlib.metadata import PackageNotFoundError
+from itertools import chain, count, repeat
+from multiprocessing import Event, Process
+from sqlite3 import IntegrityError
 
-from pytest import mark
+from pytest import mark, raises
 
-from .utils import grz_tmp, package_version
+from .utils import grz_tmp, interprocess_lock, package_version
 
 
 def test_grz_tmp_01(mocker, tmp_path):
@@ -23,6 +26,49 @@ def test_grz_tmp_01(mocker, tmp_path):
     path = grz_tmp("test1", "test2")
     assert path == fake_tmp / "test1" / "test2"
     assert path.is_dir()
+
+
+def test_interprocess_lock_basic(mocker, tmp_path):
+    """test interprocess_lock()"""
+    mocker.patch("grizzly.common.utils.perf_counter", side_effect=count())
+    mocker.patch("grizzly.common.utils.sleep", autospec=True)
+    mocker.patch("grizzly.common.utils.LOCK_DB", new=tmp_path / "lock.db")
+
+    # acquire and release
+    with interprocess_lock("test"):
+        pass
+
+    fake_conn = mocker.patch("grizzly.common.utils.connect", autospec=True).return_value
+    fake_conn.execute.side_effect = chain([None], repeat(IntegrityError))
+    # fail to acquire
+    with raises(RuntimeError, match="Failed to acquire"), interprocess_lock("test"):
+        pass
+
+
+def test_interprocess_lock_multi_proc(mocker, tmp_path):
+    """test interprocess_lock() with multiple processes"""
+    mocker.patch("grizzly.common.utils.LOCK_DB", new=tmp_path / "lock.db")
+
+    def worker(count_file, start_gate, steps):
+        start_gate.wait()
+        for _ in range(steps):
+            with interprocess_lock("test"):
+                value = int(count_file.read_text()) + 1
+                count_file.write_text(str(value))
+
+    count_file = tmp_path / "count.txt"
+    count_file.write_text("0")
+    procs_steps = 20
+    procs_count = 5
+    start_gate = Event()
+    procs = []
+    for _ in range(procs_count):
+        procs.append(Process(target=worker, args=(count_file, start_gate, procs_steps)))
+        procs[-1].start()
+    start_gate.set()
+    for proc in procs:
+        proc.join()
+    assert int(count_file.read_text()) == procs_count * procs_steps
 
 
 @mark.parametrize(
