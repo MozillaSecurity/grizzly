6151e000731be2ca57b1b6336b600fb082ce0113
Tyson Smith <tyson.w.smith@gmail.com>
2025-02-27 09:31:45 -0800

    fix(tests): update mocking in cache tests

--- src/grizzly/common/test_cache.py
+++ src/grizzly/common/test_cache.py
@@ -9,31 +9,28 @@ from .cache import _active_cache, add_cached, clear_cached, find_cached
 
 def test_cache_active_cache(mocker, tmp_path):
     """test _active_cache()"""
-    mocker.patch("grizzly.common.cache.CACHE_PATH", new=tmp_path)
-    fake_ipl = mocker.patch("grizzly.common.cache.InterProcessLock")
+    cache_path = tmp_path / "cache"
+    cache_path.mkdir()
+    mocker.patch("grizzly.common.cache.CACHE_PATH", new=cache_path)
+    mocker.patch("grizzly.common.cache.LOCK_FILE", new=tmp_path / "cache.lock")
     # create path
     mocker.patch("grizzly.common.cache._ACTIVE_CACHE", new=None)
     new_path = _active_cache()
-    assert any(tmp_path.iterdir())
-    assert tmp_path in new_path.parents
-    assert fake_ipl.call_count == 1
+    assert any(cache_path.iterdir())
+    assert cache_path in new_path.parents
     # find existing with expired entry
     mocker.patch("grizzly.common.cache._ACTIVE_CACHE", new=None)
-    (tmp_path / "0").mkdir()
+    (cache_path / "0").mkdir()
     found_path = _active_cache()
     assert found_path == new_path
-    assert fake_ipl.call_count == 2
-    # use cached value
-    found_path = _active_cache()
-    assert found_path == new_path
-    assert fake_ipl.call_count == 2
 
 
 def test_cache_basic(mocker, tmp_path):
     """test basic cache functionality"""
-    mocker.patch("grizzly.common.cache.CACHE_PATH", new=tmp_path)
-    mocker.patch("grizzly.common.cache.InterProcessLock")
     mocker.patch("grizzly.common.cache._ACTIVE_CACHE", new=None)
+    (tmp_path / "cache").mkdir()
+    mocker.patch("grizzly.common.cache.CACHE_PATH", new=tmp_path / "cache")
+    mocker.patch("grizzly.common.cache.LOCK_FILE", new=tmp_path / "cache.lock")
     # attempt to clear empty cache
     clear_cached()
     # invalid keys
@@ -68,6 +65,6 @@ def test_cache_basic(mocker, tmp_path):
     # clear with nothing expired
     clear_cached()
     assert find_cached("test-key") is not None
-    # clear with expired
+    # clear with everything expired
     clear_cached(max_age=0)
     assert find_cached("test-key") is None
