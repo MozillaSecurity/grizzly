c58f8074f94fc9c84b7743baac21a16b24f51be7
Tyson Smith <tyson.w.smith@gmail.com>
2026-03-25 11:04:07 -0700

    fix(common): split out log detection from Report

--- src/grizzly/common/test_report.py
+++ src/grizzly/common/test_report.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from FTB.Signatures.CrashInfo import CrashInfo, TraceParsingError
 from pytest import mark, raises
 
-from .report import Report
+from .report import LogMap, Report
 
 
 def _create_crash_log(log_path):
@@ -76,181 +76,6 @@ def test_report_03(tmp_path):
 
 
 def test_report_04(tmp_path):
-    """test Report._select_logs() uninteresting data"""
-    # test with empty path
-    assert Report._select_logs(tmp_path) is None
-    # empty file
-    (tmp_path / "not_a_log.txt").touch()
-    assert Report._select_logs(tmp_path) is None
-
-
-def test_report_05(tmp_path):
-    """test Report._select_logs()"""
-    # small log with nothing interesting
-    with (tmp_path / "log_asan.txt.1").open("wb") as log_fp:
-        log_fp.write(b"SHORT LOG\n")
-        log_fp.write(b"filler line")
-    # crash on another thread
-    with (tmp_path / "log_asan.txt.2").open("wb") as log_fp:
-        log_fp.write(b"GOOD LOG\n")
-        log_fp.write(
-            b"==70811==ERROR: AddressSanitizer: SEGV on unknown address 0x00000BADF00D"
-        )
-        log_fp.write(
-            b" (pc 0x7f4c0bb54c67 bp 0x7f4c07bea380 sp 0x7f4c07bea360 T0)\n"
-        )  # must be 2nd line
-        # pad out to 6 lines
-        for l_no in range(4):
-            log_fp.write(b"    #%d blah...\n" % l_no)
-    # child log that should be ignored (created when parent crashes)
-    with (tmp_path / "log_asan.txt.3").open("wb") as log_fp:
-        log_fp.write(b"BAD LOG\n")
-        log_fp.write(
-            b"==70811==ERROR: AddressSanitizer: SEGV on unknown address 0x000000000000"
-        )
-        log_fp.write(
-            b" (pc 0x7f4c0bb54c67 bp 0x7f4c07bea380 sp 0x7f4c07bea360 T2)\n"
-        )  # must be 2nd line
-        # pad out to 6 lines
-        for l_no in range(4):
-            log_fp.write(b"    #%d blah...\n" % l_no)
-    (tmp_path / "log_mindump_blah.txt").write_bytes(b"minidump log")
-    (tmp_path / "log_stderr.txt").write_bytes(b"STDERR log")
-    (tmp_path / "log_stdout.txt").write_bytes(b"STDOUT log")
-    # should be ignored in favor of "GOOD LOG"
-    (tmp_path / "log_ffp_worker_blah.txt").write_bytes(b"worker log")
-    log_map = Report._select_logs(tmp_path)
-    assert log_map
-    assert "GOOD LOG" in log_map.aux.read_text()
-    assert "STDERR" in log_map.stderr.read_text()
-    assert "STDOUT" in log_map.stdout.read_text()
-
-
-def test_report_06(tmp_path):
-    """test minidump with Report._select_logs()"""
-    (tmp_path / "log_stderr.txt").write_bytes(b"STDERR log")
-    (tmp_path / "log_stdout.txt").write_bytes(b"STDOUT log")
-    with (tmp_path / "log_minidump_01.txt").open("wb") as log_fp:
-        log_fp.write(b"GPU|||\n")
-        log_fp.write(b"Crash|SIGSEGV|0x0|0\n")
-        log_fp.write(b"minidump log\n")
-    (tmp_path / "log_ffp_worker_blah.txt").write_bytes(b"worker log")
-    log_map = Report._select_logs(tmp_path)
-    assert log_map
-    assert log_map.stderr.is_file()
-    assert log_map.stdout.is_file()
-    assert "minidump log" in log_map.aux.read_text()
-
-
-def test_report_07(tmp_path):
-    """test selecting preferred DUMP_REQUESTED minidump with Report._select_logs()"""
-    (tmp_path / "log_stderr.txt").write_bytes(b"STDERR log")
-    (tmp_path / "log_stdout.txt").write_bytes(b"STDOUT log")
-    with (tmp_path / "log_minidump_01.txt").open("wb") as log_fp:
-        log_fp.write(b"GPU|||\n")
-        log_fp.write(b"Crash|DUMP_REQUESTED|0x7f9518665d18|0\n")
-        log_fp.write(b"0|0|bar.so|sadf|a.cc:739484451a63|3066|0x0\n")
-        log_fp.write(b"0|1|gar.so|fdsa|b.cc:739484451a63|1644|0x12\n")
-    with (tmp_path / "log_minidump_02.txt").open("wb") as log_fp:
-        log_fp.write(b"GPU|||\n")
-        log_fp.write(b"Crash|DUMP_REQUESTED|0x7f57ac9e2e14|0\n")
-        log_fp.write(
-            b"0|0|foo.so|google_breakpad::ExceptionHandler::WriteMinidump|"
-            b"bar.cc:234|674|0xc\n"
-        )
-        log_fp.write(
-            b"0|1|foo.so|google_breakpad::ExceptionHandler::WriteMinidump|"
-            b"bar.cc:4a2|645|0x8\n"
-        )
-    with (tmp_path / "log_minidump_03.txt").open("wb") as log_fp:
-        log_fp.write(b"GPU|||\n")
-        log_fp.write(b"Crash|DUMP_REQUESTED|0x7f9518665d18|0\n")
-        log_fp.write(b"0|0|bar.so|sadf|a.cc:1234|3066|0x0\n")
-        log_fp.write(b"0|1|gar.so|fdsa|b.cc:4323|1644|0x12\n")
-    log_map = Report._select_logs(tmp_path)
-    assert log_map
-    assert log_map.stderr.is_file()
-    assert log_map.stdout.is_file()
-    assert "google_breakpad::ExceptionHandler::WriteMinidump" in log_map.aux.read_text()
-
-
-def test_report_08(tmp_path):
-    """test selecting worker logs with Report._select_logs()"""
-    (tmp_path / "log_stderr.txt").write_bytes(b"STDERR log")
-    (tmp_path / "log_stdout.txt").write_bytes(b"STDOUT log")
-    (tmp_path / "log_ffp_worker_1.txt").write_bytes(b"worker log")
-    # we should only ever see one but if we see multiple we warn, so test that.
-    (tmp_path / "log_ffp_worker_2.txt").write_bytes(b"worker log")
-    log_map = Report._select_logs(tmp_path)
-    assert log_map
-    assert log_map.stderr.is_file()
-    assert log_map.stdout.is_file()
-    assert "worker log" in log_map.aux.read_text()
-
-
-def test_report_09(tmp_path):
-    """test prioritizing sanitizer logs with Report._find_sanitizer()"""
-    # NOTE: ordered by selection priority in order to use previously added logs
-    # test empty
-    (tmp_path / "log_asan.txt.0").touch()
-    assert Report._find_sanitizer(list(tmp_path.iterdir())) is None
-    # test *San log with data
-    (tmp_path / "log_asan.txt.1").write_text("test")
-    selected = Report._find_sanitizer(list(tmp_path.iterdir()))
-    assert selected is not None
-    assert "test" in selected.read_text()
-    # test UBSan log
-    (tmp_path / "log_asan.txt.1").write_text(
-        "test.cc:3:5: runtime error: signed integer overflow: ..."
-    )
-    selected = Report._find_sanitizer(list(tmp_path.iterdir()))
-    assert selected is not None
-    assert "runtime error: signed integer overflow" in selected.read_text()
-    # test selecting ASan report
-    with (tmp_path / "log_asan.txt.2").open("wb") as log_fp:
-        # missing stack
-        log_fp.write(b"==1184==ERROR: AddressSanitizer: BUS on ... blah\n")
-    with (tmp_path / "log_asan.txt.3").open("wb") as log_fp:
-        log_fp.write(b"==9482==ERROR: AddressSanitizer: stack-overflow on ...\n")
-        for l_no in range(4):
-            log_fp.write(b"    #%d blah...\n" % (l_no,))
-    selected = Report._find_sanitizer(list(tmp_path.iterdir()))
-    assert selected is not None
-    assert "AddressSanitizer: stack-overflow" in selected.read_text()
-    # test selecting prioritized
-    with (tmp_path / "log_asan.txt.4").open("wb") as log_fp:
-        log_fp.write(
-            b"==1942==ERROR: AddressSanitizer: heap-use-after-free on ... blah\n"
-        )
-        for l_no in range(4):
-            log_fp.write(b"    #%d blah...\n" % (l_no,))
-    with (tmp_path / "log_asan.txt.5").open("wb") as log_fp:
-        log_fp.write(b"==1984==ERROR: AddressSanitizer: SEGV on ... blah\n")
-        log_fp.write(b"missing trace...\n")
-    with (tmp_path / "log_asan.txt.6").open("wb") as log_fp:
-        log_fp.write(b"ERROR: Failed to mmap\n")
-    selected = Report._find_sanitizer(list(tmp_path.iterdir()))
-    assert selected is not None
-    assert "heap-use-after-free" in selected.read_text()
-    # test selecting TSan reports
-    tsan_path = tmp_path / "tsan"
-    tsan_path.mkdir()
-    (tsan_path / "log_asan_benign.txt").write_text(
-        "==27531==WARNING: Symbolizer buffer too small\n"
-        "==27531==WARNING: Symbolizer buffer too small"
-    )
-    tsan_report = tsan_path / "log_asan_report.txt"
-    tsan_report.write_text(
-        "WARNING: ThreadSanitizer: data race (pid=26919)\n"
-        "  Write of size 8 at 0x7f0ca2fc3400 by thread T51:\n"
-        "    #0 memcpy /sanitizer_common_interceptors.inc:810:5 (lib+0x6656e)\n"
-    )
-    selected = Report._find_sanitizer(list(tsan_path.iterdir()))
-    assert selected is not None
-    assert selected == tsan_report
-
-
-def test_report_10(tmp_path):
     """test Report() size_limit"""
     (tmp_path / "log_stderr.txt").write_bytes(b"STDERR log\n" * 200)
     (tmp_path / "log_stdout.txt").write_bytes(b"STDOUT log\n" * 200)
@@ -273,19 +98,7 @@ def test_report_10(tmp_path):
     assert not tmp_path.is_dir()
 
 
-def test_report_11(tmp_path):
-    """test selecting Valgrind logs with Report._select_logs()"""
-    (tmp_path / "log_stderr.txt").write_bytes(b"STDERR log")
-    (tmp_path / "log_stdout.txt").write_bytes(b"STDOUT log")
-    (tmp_path / "log_valgrind.txt").write_bytes(b"valgrind log")
-    log_map = Report._select_logs(tmp_path)
-    assert log_map
-    assert log_map.stderr.is_file()
-    assert log_map.stdout.is_file()
-    assert "valgrind log" in log_map.aux.read_text()
-
-
-def test_report_12(tmp_path):
+def test_report_05(tmp_path):
     """test Report.crash_info"""
     (tmp_path / "log_stderr.txt").write_bytes(b"STDERR log")
     (tmp_path / "log_stdout.txt").write_bytes(b"STDOUT log")
@@ -310,7 +123,7 @@ def test_report_12(tmp_path):
     assert report._crash_info.configuration.product == "grizzly-test"
 
 
-def test_report_13(mocker, tmp_path):
+def test_report_06(mocker, tmp_path):
     """test Report.crash_info with unparsable log"""
     crash_info = mocker.patch("grizzly.common.report.CrashInfo", autospec=True)
     crash_info.fromRawCrashData.side_effect = TraceParsingError(line_no=0)
@@ -333,7 +146,7 @@ def test_report_13(mocker, tmp_path):
         (None, False),
     ],
 )
-def test_report_14(mocker, tmp_path, sig_cache, has_sig):
+def test_report_07(mocker, tmp_path, sig_cache, has_sig):
     """test Report.crash_signature and Report.crash_hash"""
     mocker.patch("grizzly.common.report.ProgramConfiguration", autospec=True)
     collector = mocker.patch("grizzly.common.report.Collector", autospec=True)
@@ -377,7 +190,7 @@ def test_report_14(mocker, tmp_path, sig_cache, has_sig):
         (("1", "2", "3", "4", "5", "std::panicking::rust_panic"), 5),
     ],
 )
-def test_report_15(mocker, backtrace, lines):
+def test_report_08(mocker, backtrace, lines):
     """test Report.crash_signature_max_frames()"""
     info = mocker.Mock(spec=CrashInfo, backtrace=backtrace)
     assert Report.crash_signature_max_frames(info, suggested_frames=5) == lines
@@ -396,7 +209,7 @@ def test_report_15(mocker, backtrace, lines):
         ("a\n\0", 2),
     ],
 )
-def test_report_16(tmp_path, data, lines):
+def test_report_09(tmp_path, data, lines):
     """test Report._load_log()"""
     log = tmp_path / "test-log.txt"
     log.write_text(data)
@@ -414,7 +227,7 @@ def test_report_16(tmp_path, data, lines):
         (True, True, "Potential hang detected"),
     ],
 )
-def test_report_17(mocker, tmp_path, hang, has_log, expected):
+def test_report_10(mocker, tmp_path, hang, has_log, expected):
     """test Report.short_signature"""
     mocker.patch("grizzly.common.report.ProgramConfiguration", autospec=True)
     collector = mocker.patch("grizzly.common.report.Collector", autospec=True)
@@ -428,3 +241,182 @@ def test_report_17(mocker, tmp_path, hang, has_log, expected):
     assert report.short_signature == expected
     if hang:
         assert report.crash_hash == "hang"
+
+
+def test_log_map_01(tmp_path):
+    """test LogMap.create()"""
+    # small log with nothing interesting
+    with (tmp_path / "log_asan.txt.1").open("wb") as log_fp:
+        log_fp.write(b"SHORT LOG\n")
+        log_fp.write(b"filler line")
+    # crash on another thread
+    with (tmp_path / "log_asan.txt.2").open("wb") as log_fp:
+        log_fp.write(b"GOOD LOG\n")
+        log_fp.write(
+            b"==70811==ERROR: AddressSanitizer: SEGV on unknown address 0x00000BADF00D"
+        )
+        log_fp.write(
+            b" (pc 0x7f4c0bb54c67 bp 0x7f4c07bea380 sp 0x7f4c07bea360 T0)\n"
+        )  # must be 2nd line
+        # pad out to 6 lines
+        for l_no in range(4):
+            log_fp.write(b"    #%d blah...\n" % l_no)
+    # child log that should be ignored (created when parent crashes)
+    with (tmp_path / "log_asan.txt.3").open("wb") as log_fp:
+        log_fp.write(b"BAD LOG\n")
+        log_fp.write(
+            b"==70811==ERROR: AddressSanitizer: SEGV on unknown address 0x000000000000"
+        )
+        log_fp.write(
+            b" (pc 0x7f4c0bb54c67 bp 0x7f4c07bea380 sp 0x7f4c07bea360 T2)\n"
+        )  # must be 2nd line
+        # pad out to 6 lines
+        for l_no in range(4):
+            log_fp.write(b"    #%d blah...\n" % l_no)
+    (tmp_path / "log_mindump_blah.txt").write_bytes(b"minidump log")
+    (tmp_path / "log_stderr.txt").write_bytes(b"STDERR log")
+    (tmp_path / "log_stdout.txt").write_bytes(b"STDOUT log")
+    # should be ignored in favor of "GOOD LOG"
+    (tmp_path / "log_ffp_worker_blah.txt").write_bytes(b"worker log")
+    log_map = LogMap.create(tmp_path)
+    assert all(log_map)
+    assert "GOOD LOG" in log_map.aux.read_text()
+    assert "STDERR" in log_map.stderr.read_text()
+    assert "STDOUT" in log_map.stdout.read_text()
+
+
+def test_log_map_02(tmp_path):
+    """test minidump with LogMap.create()"""
+    (tmp_path / "log_stderr.txt").write_bytes(b"STDERR log")
+    (tmp_path / "log_stdout.txt").write_bytes(b"STDOUT log")
+    with (tmp_path / "log_minidump_01.txt").open("wb") as log_fp:
+        log_fp.write(b"GPU|||\n")
+        log_fp.write(b"Crash|SIGSEGV|0x0|0\n")
+        log_fp.write(b"minidump log\n")
+    (tmp_path / "log_ffp_worker_blah.txt").write_bytes(b"worker log")
+    log_map = LogMap.create(tmp_path)
+    assert log_map.stderr.is_file()
+    assert log_map.stdout.is_file()
+    assert "minidump log" in log_map.aux.read_text()
+
+
+def test_log_map_03(tmp_path):
+    """test selecting preferred DUMP_REQUESTED minidump with LogMap.create()"""
+    (tmp_path / "log_stderr.txt").write_bytes(b"STDERR log")
+    (tmp_path / "log_stdout.txt").write_bytes(b"STDOUT log")
+    with (tmp_path / "log_minidump_01.txt").open("wb") as log_fp:
+        log_fp.write(b"GPU|||\n")
+        log_fp.write(b"Crash|DUMP_REQUESTED|0x7f9518665d18|0\n")
+        log_fp.write(b"0|0|bar.so|sadf|a.cc:739484451a63|3066|0x0\n")
+        log_fp.write(b"0|1|gar.so|fdsa|b.cc:739484451a63|1644|0x12\n")
+    with (tmp_path / "log_minidump_02.txt").open("wb") as log_fp:
+        log_fp.write(b"GPU|||\n")
+        log_fp.write(b"Crash|DUMP_REQUESTED|0x7f57ac9e2e14|0\n")
+        log_fp.write(
+            b"0|0|foo.so|google_breakpad::ExceptionHandler::WriteMinidump|"
+            b"bar.cc:234|674|0xc\n"
+        )
+        log_fp.write(
+            b"0|1|foo.so|google_breakpad::ExceptionHandler::WriteMinidump|"
+            b"bar.cc:4a2|645|0x8\n"
+        )
+    with (tmp_path / "log_minidump_03.txt").open("wb") as log_fp:
+        log_fp.write(b"GPU|||\n")
+        log_fp.write(b"Crash|DUMP_REQUESTED|0x7f9518665d18|0\n")
+        log_fp.write(b"0|0|bar.so|sadf|a.cc:1234|3066|0x0\n")
+        log_fp.write(b"0|1|gar.so|fdsa|b.cc:4323|1644|0x12\n")
+    log_map = LogMap.create(tmp_path)
+    assert log_map.stderr.is_file()
+    assert log_map.stdout.is_file()
+    assert "google_breakpad::ExceptionHandler::WriteMinidump" in log_map.aux.read_text()
+
+
+def test_log_map_04(tmp_path):
+    """test selecting worker logs with LogMap.create()"""
+    (tmp_path / "log_stderr.txt").write_bytes(b"STDERR log")
+    (tmp_path / "log_stdout.txt").write_bytes(b"STDOUT log")
+    (tmp_path / "log_ffp_worker_1.txt").write_bytes(b"worker log")
+    # we should only ever see one but if we see multiple we warn, so test that.
+    (tmp_path / "log_ffp_worker_2.txt").write_bytes(b"worker log")
+    log_map = LogMap.create(tmp_path)
+    assert log_map.stderr.is_file()
+    assert log_map.stdout.is_file()
+    assert "worker log" in log_map.aux.read_text()
+
+
+def test_log_map_05(tmp_path):
+    """test prioritizing sanitizer logs with LogMap._find_sanitizer()"""
+    # NOTE: ordered by selection priority in order to use previously added logs
+    # test empty
+    (tmp_path / "log_asan.txt.0").touch()
+    assert LogMap._find_sanitizer(list(tmp_path.iterdir())) is None
+
+    # test *San log with data
+    (tmp_path / "log_asan.txt.1").write_text("test")
+    selected = LogMap._find_sanitizer(list(tmp_path.iterdir()))
+    assert selected is not None
+    assert "test" in selected.read_text()
+    # test UBSan log
+    (tmp_path / "log_asan.txt.1").write_text(
+        "test.cc:3:5: runtime error: signed integer overflow: ..."
+    )
+    selected = LogMap._find_sanitizer(list(tmp_path.iterdir()))
+    assert selected is not None
+    assert "runtime error: signed integer overflow" in selected.read_text()
+
+    # test selecting ASan report
+    with (tmp_path / "log_asan.txt.2").open("wb") as log_fp:
+        # missing stack
+        log_fp.write(b"==1184==ERROR: AddressSanitizer: BUS on ... blah\n")
+    with (tmp_path / "log_asan.txt.3").open("wb") as log_fp:
+        log_fp.write(b"==9482==ERROR: AddressSanitizer: stack-overflow on ...\n")
+        for l_no in range(4):
+            log_fp.write(b"    #%d blah...\n" % (l_no,))
+    selected = LogMap._find_sanitizer(list(tmp_path.iterdir()))
+    assert selected is not None
+    assert "AddressSanitizer: stack-overflow" in selected.read_text()
+
+    # test selecting prioritized
+    with (tmp_path / "log_asan.txt.4").open("wb") as log_fp:
+        log_fp.write(
+            b"==1942==ERROR: AddressSanitizer: heap-use-after-free on ... blah\n"
+        )
+        for l_no in range(4):
+            log_fp.write(b"    #%d blah...\n" % (l_no,))
+    with (tmp_path / "log_asan.txt.5").open("wb") as log_fp:
+        log_fp.write(b"==1984==ERROR: AddressSanitizer: SEGV on ... blah\n")
+        log_fp.write(b"missing trace...\n")
+    with (tmp_path / "log_asan.txt.6").open("wb") as log_fp:
+        log_fp.write(b"ERROR: Failed to mmap\n")
+    selected = LogMap._find_sanitizer(list(tmp_path.iterdir()))
+    assert selected is not None
+    assert "heap-use-after-free" in selected.read_text()
+
+    # test selecting TSan reports
+    tsan_path = tmp_path / "tsan"
+    tsan_path.mkdir()
+    (tsan_path / "log_asan_benign.txt").write_text(
+        "==27531==WARNING: Symbolizer buffer too small\n"
+        "==27531==WARNING: Symbolizer buffer too small"
+    )
+    tsan_report = tsan_path / "log_asan_report.txt"
+    tsan_report.write_text(
+        "WARNING: ThreadSanitizer: data race (pid=26919)\n"
+        "  Write of size 8 at 0x7f0ca2fc3400 by thread T51:\n"
+        "    #0 memcpy /sanitizer_common_interceptors.inc:810:5 (lib+0x6656e)\n"
+    )
+    selected = LogMap._find_sanitizer(list(tsan_path.iterdir()))
+    assert selected is not None
+    assert selected == tsan_report
+
+
+def test_log_map_06(tmp_path):
+    """test selecting Valgrind logs with LogMap.create()"""
+    (tmp_path / "log_stderr.txt").write_bytes(b"STDERR log")
+    (tmp_path / "log_stdout.txt").write_bytes(b"STDOUT log")
+    (tmp_path / "log_valgrind.txt").write_bytes(b"valgrind log")
+    log_map = LogMap.create(tmp_path)
+    assert log_map
+    assert log_map.stderr.is_file()
+    assert log_map.stdout.is_file()
+    assert "valgrind log" in log_map.aux.read_text()
